60c6a99466135ee0efa8c1b893f52742eb6340a3
queyue.crk <queyue.crk@alibaba-inc.com>
2020-01-13 11:43:41 +0800

    fix: local prediction for detectron

--- packages/pipcook-plugins-detection-detectron-model-deploy/src/index.ts
+++ packages/pipcook-plugins-detection-detectron-model-deploy/src/index.ts
@@ -1,23 +1,25 @@
-import {DataProcessType, ArgsType, ModelDeployType, downloadZip, createAnnotationFile, DataAccessType} from '@pipcook/pipcook-core';
+import {ArgsType, ModelDeployType, downloadZip} from '@pipcook/pipcook-core';
 import * as path from 'path';
 const uuidv1 = require('uuid/v1');
 const fs = require('fs-extra');
 
 const detectionDetectronModelDeploy: ModelDeployType = async (args: ArgsType): Promise<any> => {
   const {
-    data, model
+    data='', model=''
   } = args || {};
   if (!data) {
     return;
   }
   const trainDataPath = path.join(process.cwd(), '.temp', uuidv1());
   try {
+    const images: string[] = [];
     for (let i = 0; i < data.length; i++) {
       const picName = uuidv1()+'.png'; 
       await downloadZip(data[i], path.join(trainDataPath, 'images', picName));
-      const prediction = await model.predict(path.join(trainDataPath, 'images', picName));
-      return prediction;
+      images.push(path.join(trainDataPath, 'images', picName));
     }
+    const prediction = await model.predict(images);
+    return prediction;
   } finally {
     fs.removeSync(trainDataPath);
   }
