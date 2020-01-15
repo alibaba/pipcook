6c21bf9f6e67a355c9c36c830256f7e42aae6aea
queyue.crk <queyue.crk@alibaba-inc.com>
2020-01-15 16:25:28 +0800

    feat: eas deploy for detectron detection

--- packages/pipcook-plugins-detection-detectron-eas-model-deploy/src/index.ts
+++ packages/pipcook-plugins-detection-detectron-eas-model-deploy/src/index.ts
@@ -1,21 +1,18 @@
-import {ArgsType, ModelDeployType, downloadZip, unZipData, getModelDir} from '@pipcook/pipcook-core';
+import {ArgsType, ModelDeployType, downloadZip, unZipData, getModelDir, compressTarFile, UniformGeneralSampleData, PipcookModel} from '@pipcook/pipcook-core';
 import * as path from 'path';
+import * as assert from 'assert';
+
+const OSS = require('ali-oss');
+const shell = require('shelljs');
 const uuidv1 = require('uuid/v1');
 const fs = require('fs-extra');
-import * as assert from 'assert';
-const targz = require('tar.gz');
-const ncp = require('ncp').ncp;
-const OSS = require('ali-oss');
-var shell = require('shelljs');
 
-const detectionDetectronModelDeploy: ModelDeployType = async (args: ArgsType): Promise<any> => {
+
+const detectionDetectronModelDeploy: ModelDeployType = async (data: UniformGeneralSampleData, model: PipcookModel, args: ArgsType): Promise<any> => {
   const {
-    data='', model='', modelId='', easName='', cpus=8, memory=12000, instances=1, ossConfig={}, ossDir=''
+    easName='', cpus=2, memory=4000, ossConfig={}, ossDir=''
   } = args || {};
-  if (!data) {
-    return;
-  }
-  const packagePath = path.join(process.cwd(), '.temp', uuidv1());
+  const packagePath = path.join(process.cwd(), '.temp1', uuidv1());
   fs.ensureDirSync(path.join(packagePath, easName))
   try {
     // get detectron env
@@ -26,45 +23,42 @@ const detectionDetectronModelDeploy: ModelDeployType = async (args: ArgsType): P
     fs.removeSync(zipPath)
     // write app.json
     const app = {
-      processor_entry: 'http://' + ossConfig.bucket +'.'+ossConfig.region+'.aliyuncs.com/'+ossDir+'/'+easName+'.tar.gz',
+      processor_path: 'http://' + ossConfig.bucket +'.'+ossConfig.region+'.aliyuncs.com/'+ossDir+'/'+easName+'.tar.gz',
+      processor_entry: './app.py',
       processor_type: "python",
       name: easName,
-      generate_token: true,
+      generate_token: "true",
       metadata: {
         cpu: cpus,
         memory: memory,
         "rpc.keepalive": 60000
       }
     }
-    fs.writeFileSync(path.join(packagePath, easName, 'app.json'), JSON.stringify(app));
+    fs.outputFileSync(path.join(packagePath, easName, 'app.json'), JSON.stringify(app));
     // copy config
-    const configPath = '' // TODO
-    ncp(configPath, path.join(packagePath, easName), function (err) {
-      if (err) {
-        throw err;
-      }
-    })
+    const configPath = model.extraParams.detectronConfigPath
+    fs.copySync(configPath, path.join(packagePath, easName, 'config'));
+
     // copy model
-    const modelPath = modelId ? path.join(getModelDir(modelId), 'model_final.pth'):path.join(process.cwd(), '.temp', 'output', 'model_final.pth');
-    fs.ensureDirSync(path.join(packagePath, easName, 'output'))
-    fs.copyFile(modelPath, path.join(packagePath, easName, 'output', 'model_final.pth'), (err) => {
-      if (err) throw err;
-    });
+    const modelPath = model.extraParams.modelPath;
+    fs.copySync(modelPath, path.join(packagePath, easName, 'output', 'model_final.pth'));
+
     // copy app.py
     await downloadZip(envUrl + 'app.py', path.join(packagePath, easName, 'app.py'));
 
     // save label map
     fs.writeFileSync(path.join(packagePath, easName, 'label.json'), JSON.stringify(data.metaData.label.valueMap));
+
     // package the whole content
-    var read = targz().createReadStream(path.join(packagePath, easName));
-    var write = fs.createWriteStream(path.join(packagePath, easName + '.tar.gz'));
-    read.pipe(write);
+    await compressTarFile(path.join(packagePath, easName), path.join(packagePath, easName + '.tar.gz'));
+
     // upload to oss
-    const client = OSS(ossConfig)
-    await client.put(path.join(ossDir, easName + '.tar.gz'), path.join(packagePath, easName + '.tar.gz'));
+    const client = OSS(ossConfig);
+    await client.put(path.join(ossDir, easName + '.tar.gz'), path.join(packagePath, easName + '.tar.gz'), {timeout: 60000000});
+
     // create service
     assert.ok(shell.which('eascmd'), 'please install eascmd first');
-    if (shell.exec('eascmd create' + path.join(packagePath, easName, 'app.json')).code !== 0) {
+    if (shell.exec('eascmd create ' + path.join(packagePath, easName, 'app.json')).code !== 0) {
       shell.echo('Error: create service' + easName + 'failed');
       shell.exit(1);
     }
