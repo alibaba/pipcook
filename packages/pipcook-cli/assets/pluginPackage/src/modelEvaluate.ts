238115494efc5e37c0d95cb0be900c1b7d5acfaa
Yorkie Liu <yorkiefixer@gmail.com>
2020-02-19 15:25:28 +0800

    cli: fix the type casts by `as` keyword (#12)
    The cast in <> is not recommended because it's conflicts with jsx/tsx's
    syntax.
    
    See:
    
    https://palantir.github.io/tslint/rules/no-angle-bracket-type-assertion/
    https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/consistent-type-assertions.md

--- packages/pipcook-cli/assets/pluginPackage/src/modelEvaluate.ts
+++ packages/pipcook-cli/assets/pluginPackage/src/modelEvaluate.ts
@@ -2,7 +2,7 @@
 import {PipcookModel, UniformSampleData, ArgsType, ModelEvaluateType, EvaluateResult} from '@pipcook/pipcook-core';
 
 const templateModelEvaluate: ModelEvaluateType = async (data: UniformSampleData, model: PipcookModel, args?: ArgsType): Promise<EvaluateResult> => {
-  return <EvaluateResult>{};
+  return {} as EvaluateResult;
 }
 
 export default templateModelEvaluate;
