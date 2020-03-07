17eaf8f17c5761155454c755ed00a452c422af3b
yorkie <yorkiefixer@gmail.com>
2020-03-07 20:22:27 +0800

    utils: refactor the logger in more strict types, and add tests

--- packages/pipcook-core/src/utils/serverProcess.ts
+++ packages/pipcook-core/src/utils/serverProcess.ts
@@ -1,13 +1,14 @@
-import * as path from "path";
-const fs = require("fs-extra");
-import {PipcookComponentResult} from '../types/component';
+import * as path from 'path';
+import * as fs from 'fs-extra';
 import {DataAccessType, DataProcessType} from '../types/plugins';
-import {createAnnotationFile, createAnnotationFromJson} from './publicUtils';
+import {PipcookComponentResult} from '../types/component';
 import {IMAGE_CLASSIFICATION, OBJECT_DETECTION} from '../constants/model';
-import {transformCsv} from '../utils/publicUtils';
+import {createAnnotationFile, createAnnotationFromJson, transformCsv} from './publicUtils';
 
-export async function processImageClassification(data: any, dataAccess: PipcookComponentResult, 
-  dataProcess: PipcookComponentResult, type: 'image classification' | 'object detection') {
+export async function processImageClassification(data: any[],
+  dataAccess: PipcookComponentResult,
+  dataProcess: PipcookComponentResult,
+  type: 'image classification' | 'object detection') {
   // all image will be stored to a temporary directory
   const trainDataPath = path.join(process.cwd(), '.temp');
   try {
@@ -51,53 +52,54 @@ export async function processImageClassification(data: any, dataAccess: PipcookC
     }
     result = result.trainData.map((e: any) => {
       return e.xs;
-    })
+    });
     result = result.batch(data.length);
     result = result.take(1);
     return result;
   } catch(err) {
     console.error(err);
-  }finally {
+  } finally {
     fs.removeSync(trainDataPath);
   }
 }
 
-export async function processTextClassification(data: any, dataAccess: PipcookComponentResult, 
+export async function processTextClassification(
+  data: any[],
+  dataAccess: PipcookComponentResult, 
   dataProcess: PipcookComponentResult) {
-    const trainDataPath = path.join(process.cwd(), '.temp');
-    try {
-      const trainData: string[] = [];
-      data.forEach((dataText: any) => {
-        trainData.push(`${transformCsv(dataText)},temp`);
-      });
-      const finalText = trainData.join('\n');
-      fs.outputFileSync(path.join(trainDataPath, 'train.csv'), finalText);
-  
-      let result: any = {
-        trainDataPath: path.join(trainDataPath, 'train.csv'),
-      }
-      const dataAccessPlugin = dataAccess.plugin as DataAccessType;
-      result = await dataAccessPlugin(result, dataAccess.params);
-      if (dataProcess) {
-        const dataProcessPlugin = dataProcess.plugin as DataProcessType;
-        result = await dataProcessPlugin(result, dataProcess.params);
-      }
-      result = result.trainData.map((e: any) => {
-        return e.xs;
-      })
-      result = result.take(1);
-      return result;
-    }finally {
-      fs.removeSync(trainDataPath);
+  const trainDataPath = path.join(process.cwd(), '.temp');
+  try {
+    const trainData: string[] = [];
+    data.forEach((dataText: any) => {
+      trainData.push(`${transformCsv(dataText)},temp`);
+    });
+    const finalText = trainData.join('\n');
+    fs.outputFileSync(path.join(trainDataPath, 'train.csv'), finalText);
+
+    let result: any = {
+      trainDataPath: path.join(trainDataPath, 'train.csv'),
     }
+    const dataAccessPlugin = dataAccess.plugin as DataAccessType;
+    result = await dataAccessPlugin(result, dataAccess.params);
+    if (dataProcess) {
+      const dataProcessPlugin = dataProcess.plugin as DataProcessType;
+      result = await dataProcessPlugin(result, dataProcess.params);
+    }
+    result = result.trainData.map((e: any) => {
+      return e.xs;
+    })
+    result = result.take(1);
+    return result;
+  } finally {
+    fs.removeSync(trainDataPath);
   }
+}
 
 export function serverStatic(fastify: any) {
   fastify.register(require('fastify-static'), {
     root: path.join(__dirname, '..' ,'assets', 'build'),
   });
-  console.log('here')
   fastify.get('/index', function (req: any, reply: any) {
-    reply.sendFile('index.html') 
-  })
+    reply.sendFile('index.html');
+  });
 }
