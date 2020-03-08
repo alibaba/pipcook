b1cf58da2e99212d3000fa276ecb772d63f67155
Yorkie Liu <yorkiefixer@gmail.com>
2020-03-08 14:47:30 +0800

    core: add tests for constants (#37)

---
+++ packages/pipcook-core/src/constants/other_test.ts
@@ -0,0 +1,18 @@
+import * as other from './other';
+
+describe('other constants', () => {
+  it('should own some constants', () => {
+    expect(other.STATUS_NOT_EXECUTE).toEqual('not execute');
+    expect(other.STATUS_SUCCESS).toEqual('success');
+    expect(other.STATUS_FAILURE).toEqual('failure');
+    expect(other.STATUS_RUNNING).toEqual('running');
+
+    expect(other.DATA).toEqual('data');
+    expect(other.MODEL).toEqual('model');
+    expect(other.EVALUATE).toEqual('evaluate');
+    expect(other.DEPLOYMENT).toEqual('deployment');
+    expect(other.MERGE).toEqual('merge');
+    expect(other.MODELTOSAVE).toEqual('modeltosave');
+    expect(other.ORIGINDATA).toEqual('origindata');
+  });
+});
