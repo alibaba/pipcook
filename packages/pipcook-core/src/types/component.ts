218af5362fca8acdc782691d36665625c110e561
Yorkie Liu <yorkiefixer@gmail.com>
2020-03-08 14:49:04 +0800

    core: remove the unused interface and fix type (#38)

--- packages/pipcook-core/src/types/component.ts
+++ packages/pipcook-core/src/types/component.ts
@@ -4,9 +4,6 @@ import {OriginSampleData, UniformSampleData, InsertParams} from './data';
 import {PipObject} from './other';
 import {Subscribable} from 'rxjs';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface PipcookComponent {}
-
 interface ObserverFunc {
   (data: OriginSampleData | OriginSampleData[]| UniformSampleData | UniformSampleData[] | null, 
     model: PipcookModel | PipcookModel[] |null, insertParams: InsertParams): Subscribable<any>;
@@ -24,7 +21,7 @@ type ResultType =
 export interface PipcookComponentResult {
   type: ResultType;
   plugin?: PipcookPlugin;
-  mergeComponents?: PipcookComponent[][];
+  mergeComponents?: PipcookComponentResult[][];
   params?: PipObject;
   observer?: ObserverFunc;
   returnType: string;
@@ -32,10 +29,6 @@ export interface PipcookComponentResult {
   status: 'not execute' | 'running' | 'success' | 'failure';
 }
 
-export interface PipcookLifeCycleComponent extends PipcookComponent{
+export interface PipcookLifeCycleComponent {
   (plugin: PipcookPlugin, params?: PipObject): PipcookComponentResult;
 }
-
-// export interface DataMergeComponent extends PipcookComponent {
-//   (...mergeComponents: PipcookComponentResult[][]): PipcookComponentResult;
-// }
