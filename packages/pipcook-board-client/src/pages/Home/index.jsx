f6a537d030baf5cc67d47e399541fd2d043f7697
queyue.crk <queyue.crk@alibaba-inc.com>
2020-03-19 21:25:19 +0800

    change package.json and tfconfig.json accordingly

--- packages/pipcook-board-client/src/pages/Home/index.jsx
+++ packages/pipcook-board-client/src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ export default function Home () {
     return (
       <div className="home">
         {/* <div dangerouslySetInnerHTML={{__html:document}} /> */}
-        <iframe title="doc" src="https://alibaba.github.io/pipcook/doc/What%20is%20Pipcook%3F-en" style={{minWidth: '100%', minHeight: '100vh'}}/>
+        <iframe title="doc" src="https://alibaba.github.io/pipcook/#/" style={{minWidth: '100%', minHeight: '100vh'}}/>
       </div>
     );  
 }
