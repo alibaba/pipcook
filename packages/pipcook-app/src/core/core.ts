0a7ca28470fb0f75ce98be0b841c69df1d7c7cd7
queyue.crk <queyue.crk@alibaba-inc.com>
2020-04-07 14:59:06 +0800

    fix lerna install

--- packages/pipcook-app/src/core/core.ts
+++
@@ -1,9 +0,0 @@
-import ImageClassification from '../models/imageClassification';
-import ObjectDetection from '../models/objectDetection';
-import TextClassification from '../models/textClassification';
-
-export default class Pipcook {
-  imageClassification = ImageClassification;
-  objectDetection = ObjectDetection;
-  textClassification = TextClassification;
-}
