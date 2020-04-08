12adda84ec7f94226bdc1443dc48621eb8442a05
queyue.crk <queyue.crk@alibaba-inc.com>
2020-04-08 15:25:12 +0800

    refractor scope name

---
+++ packages/cli/assets/server/app/router.ts
@@ -0,0 +1,14 @@
+import { Application } from 'egg';
+
+export default (app: Application) => {
+  const { controller, router } = app;
+
+  // logs
+  router.get('/log/logs', controller.log.logs);
+  router.get('/log/datasets', controller.log.datasets);
+  router.get('/log/models', controller.log.models);
+
+  // showcase
+  router.post('/showcase/mnist', controller.showcase.mnist);
+  router.post('/showcase/asset-classification', controller.showcase.assetClassification);
+};
---
+++ packages/cli/assets/server/typings/app/service/index.d.ts
@@ -0,0 +1,15 @@
+// This file is created by egg-ts-helper@1.25.7
+// Do not modify this file!!!!!!!!!
+
+import 'egg';
+type AnyClass = new (...args: any[]) => any;
+type AnyFunc<T = any> = (...args: any[]) => T;
+type CanExportFunc = AnyFunc<Promise<any>> | AnyFunc<IterableIterator<any>>;
+type AutoInstanceType<T, U = T extends CanExportFunc ? T : T extends AnyFunc ? ReturnType<T> : T> = U extends AnyClass ? InstanceType<U> : U;
+import ExportTest from '../../../app/service/Test';
+
+declare module 'egg' {
+  interface IService {
+    test: AutoInstanceType<typeof ExportTest>;
+  }
+}
