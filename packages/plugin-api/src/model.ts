2063d7a31629cf3e28e9ae1b66664defaba5d843
queyue.crk <queyue.crk@alibaba-inc.com>
2020-04-13 16:03:19 +0800

    move boa to file scope

--- packages/plugin-api/src/model.ts
+++ packages/plugin-api/src/model.ts
@@ -1,10 +1,12 @@
-const boa = require('@pipcook/boa');
 import * as fs from 'fs-extra';
-import * as path from 'path'
+import * as path from 'path';
 
-export function gray(src: string, dest: string) {
+const boa = require('@pipcook/boa');
+const cv2 = boa.import('cv2');
+
+
+export async function gray(src: string, dest: string) {
   fs.ensureDirSync(dest);
-  const cv2 = boa.import('cv2');
   const image = cv2.imread(src);
   const gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY);
   cv2.imwrite(path.join(dest, path.basename(src)), gray);
