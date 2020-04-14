fc03ab82acd6c1f84824365a6e92cc103720c951
hujing.hujing <hujing.hujing@alibaba-inc.com>
2020-04-14 22:23:54 +0800

    add resize and tokenize

--- packages/plugin-api/src/model.ts
+++ packages/plugin-api/src/model.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 
 const boa = require('@pipcook/boa');
 const cv2 = boa.import('cv2');
+const {word_tokenize} = boa.import('nltk.tokenize')
 
 
 export async function gray(src: string, dest: string) {
@@ -10,4 +11,15 @@ export async function gray(src: string, dest: string) {
   const image = cv2.imread(src);
   const gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY);
   cv2.imwrite(path.join(dest, path.basename(src)), gray);
+}
+
+export async function resize(src: string, dest: string, dim: [number,number]) {
+  fs.ensureDirSync(dest);
+  const image = cv2.imread(src);
+  const resized = cv2.resize(image, dim);
+  cv2.imwrite(path.join(dest, path.basename(src)), resized);
+}
+
+export function tokenize(sentence: string) {
+  return word_tokenize(sentence);
 }
