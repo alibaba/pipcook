4bc89735066b6af1e5df06804be1ca3d7726d864
Txiaozhe <txiaozhe@gmail.com>
2020-04-14 22:00:40 +0800

    optimize npm client config.

--- packages/cli/lib/init.js
+++ packages/cli/lib/init.js
@@ -6,7 +6,7 @@ const glob = require('glob-promise');
 const commandExistsSync = require('command-exists').sync;
 const inquirer = require('inquirer');
 
-const { dependencies, pipcookLogName } = require('./config');
+const { dependencies, pipcookLogName, optionalNpmClients } = require('./config');
 const spinner = ora();
 
 /**
@@ -16,17 +16,18 @@ const init = async (cmdObj) => {
   let client = 'npm';
   if (cmdObj && cmdObj[0]) {
     client = cmdObj[0];
+    if (!optionalNpmClients.includes(client)) {
+      spinner.fail(`Invalid npm client: ${client}.`);
+      return;
+    }
   } else {
     const clientChoices = [];
-    if (commandExistsSync('npm')) {
-      clientChoices.push('npm');
+    for (const npmClient of optionalNpmClients) {
+      if (commandExistsSync(npmClient)) {
+        clientChoices.push(npmClient);
+      }
     }
-    if (commandExistsSync('cnpm')) {
-      clientChoices.push('cnpm');
-    }
-    if (commandExistsSync('tnpm')) {
-      clientChoices.push('tnpm');
-    }
+
     if (clientChoices.length === 1) {
       client = clientChoices[0];
     } else if (clientChoices.length > 1) {
