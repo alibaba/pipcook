ace4048a20baa139ef3503c0128c4209540d8ae1
queyue.crk <queyue.crk@alibaba-inc.com>
2020-04-20 10:42:27 +0800

    update dependencies

--- packages/boa/tools/install-python.js
+++ packages/boa/tools/install-python.js
@@ -27,7 +27,7 @@ if (!fs.existsSync(condaDownloadName)) {
   run(`curl ${CONDA_DOWNLOAD_PREFIX}/${condaDownloadName} > ${condaDownloadName}`);
 }
 
-if (!(fs.existsSync(CONDA_LOCAL_PATH) && fs.existsSync(path.join(CONDA_LOCAL_PATH, 'bin', 'python')))) {
+if (!fs.existsSync(path.join(CONDA_LOCAL_PATH, 'bin', 'python'))) {
   run('rm', `-rf ${CONDA_LOCAL_PATH}`);
   run('sh', `./${condaDownloadName}`, `-f -b -p ${CONDA_LOCAL_PATH}`);
   run('rm', `-rf ${CONDA_LOCAL_PATH}/lib/libstdc++.so*`);
