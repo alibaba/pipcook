8e15a1eb25b5501027205f46ec1fd691913f6bf5
Ruikun Cao <queyue.crk@alibaba-inc.com>
2020-04-20 21:38:15 +0800

    boa: do not install conda again when it's existed (#100)

--- example/boa/tf2/hello.js
+++ example/boa/tf2/hello.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const boa = require('@pipcook/boa');
 const tf = boa.import('tensorflow');
@@ -6,5 +6,5 @@ const tf = boa.import('tensorflow');
 const n1 = tf.add(1, 2).numpy();
 console.log(n1); // 3
 
-const n2 = tf.constant('Hello, TensorFlow!').numpy()
+const n2 = tf.constant('Hello, TensorFlow!').numpy();
 console.log(n2); // b'Hello, TensorFlow!'
