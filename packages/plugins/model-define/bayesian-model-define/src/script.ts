b8e09d7a6bee25d7de334fbcf23d60ae69e0960b
Txiaozhe <txiaozhe@gmail.com>
2020-04-21 10:59:59 +0800

    fixup.

--- packages/plugins/model-define/bayesian-model-define/src/script.ts
+++ packages/plugins/model-define/bayesian-model-define/src/script.ts
@@ -1,44 +1,45 @@
 'use strict';
 
+import fs from 'fs';
+import readline from 'readline';
+
 const boa = require('@pipcook/boa');
 
 const jieba = boa.import('jieba');
 const { MultinomialNB } = boa.import('sklearn.naive_bayes');
 const pickle = boa.import('pickle');
 
-const { open } = boa.builtins();
-
-const fs = require('fs');
-const readline = require('readline');
-
-function strip (str) {
+function strip(str: string): string {
   return str.replace(/(^\s*)|(\s*$)/g, '');
 }
 
-function MakeWordsSet(words_file) {
-  const words_set = new Set();
-  const rs = fs.createReadStream(words_file);
+function isdigit(str: string): boolean {
+  return /^\d+$/.test(str);
+}
+
+function MakeWordsSet(words_file: string): Promise<Set<string>> {
+  const words_set = new Set<string>();
   const rl = readline.createInterface({
-    input: rs
+    input: fs.createReadStream(words_file)
   });
   return new Promise((resolve) => {
-    rl.on('line', function (line) {
+    rl.on('line', (line: string) => {
       const word = strip(line);
       if (word.length > 0 && !words_set.has(word)) {
         words_set.add(word);
       }
     });
 
-    rl.on('close', function () {
+    rl.on('close', () => {
       resolve(words_set);
     });
   });
 }
 
-function words_dict(all_words_list, stopwords_set = new Set()) {
-  const feature_words = [];
+function words_dict(all_words_list: string[], stopwords_set = new Set<string>()): string[] {
+  const feature_words: string[] = [];
   for (let word of all_words_list) {
-    if (!word.isdigit() &&
+    if (!isdigit(word) &&
         !stopwords_set.has(word) &&
         word.length > 1 &&
         word.length < 5) {
@@ -49,14 +50,14 @@ function words_dict(all_words_list, stopwords_set = new Set()) {
   return feature_words;
 }
 
-exports.processPredictData = function (data, all_words_list_path, stopwords_path) {
-  const all_words_list = pickle.load(open(all_words_list_path, 'rb'));
+export const processPredictData = function (data: any, all_words_list_path: string, stopwords_path: string): Promise<Set<string>> {
+  const all_words_list = pickle.load(fs.readFileSync(all_words_list_path));
   const word_cut = jieba.cut(data, boa.kwargs({
     cut_all: false
   }));
 
   const stopwords_file = stopwords_path;
-  const text_features = function (text, feature_words) {
+  const text_features = function (text: string, feature_words: string[]): number[] {
     const text_words = new Set(text);
     const features = [];
     for (let word of feature_words) {
@@ -72,18 +73,16 @@ exports.processPredictData = function (data, all_words_list_path, stopwords_path
 
   return MakeWordsSet(stopwords_file).then((stopwords_set) => {
     const feature_words = words_dict(all_words_list, stopwords_set);
-    return word_cut.map((text) => {
+    return word_cut.map((text: string) => {
       return text_features(text, feature_words);
     });
   });
 };
 
-exports.getBayesModel = function () {
-  const classifier = MultinomialNB();
-  return classifier;
+export const getBayesModel = function (): any {
+  return MultinomialNB();
 };
 
-exports.loadModel = function (filepath) {
-  const classifier = pickle.load(open(filepath, 'rb'));
-  return classifier;
+export const loadModel = function (filepath: string): any {
+  return pickle.load(fs.readFileSync(filepath));
 };
