cde101a547786d2747e6b1f7be7b601f9856c42f
Yorkie Liu <yorkiefixer@gmail.com>
2020-04-27 16:58:44 +0800

    plugin, core: fail the pipeline when evaluation is not pass (#133)
    * plugin, core: fail the pipeline when evaluation is not pass

--- packages/core/src/types/other.ts
+++ packages/core/src/types/other.ts
@@ -15,7 +15,16 @@ export interface PipObject {
 }
 
 export interface EvaluateResult {
+  pass?: boolean;
   [key: string]: any;
+}
+
+export class EvaluateError extends TypeError {
+  public result: EvaluateResult;
+  constructor(r: EvaluateResult) {
+    super(`the evaluate result is not pass, ${JSON.stringify(r, null, 2)}`);
+    this.result = r;
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
