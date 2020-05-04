6b93257eedacbc5c2b847f4074ba867916fcc432
Yorkie Liu <yorkiefixer@gmail.com>
2020-05-04 22:50:24 +0800

    fix nits

--- packages/boa/tools/install-requirements.js
+++ packages/boa/tools/install-requirements.js
@@ -4,4 +4,3 @@ const path = require('path');
 const { pip } = require('./utils');
 
 pip('install', `-r ${path.join(__dirname, '../requirements.txt')}`);
-pip('install', ' --upgrade --force-reinstall numpy');
