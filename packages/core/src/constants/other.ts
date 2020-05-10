b7cd3fe8896607de86cb3b566c82bf4d93611aa7
Jory Liang <liangzr@outlook.com>
2020-05-10 10:54:45 +0800

    core: add type definition of the ouput type (#185)

--- packages/core/src/constants/other.ts
+++ packages/core/src/constants/other.ts
@@ -1,11 +1,9 @@
-export const STATUS_NOT_EXECUTE = 'not execute';
-export const STATUS_SUCCESS = 'success';
-export const STATUS_FAILURE = 'failure';
-export const STATUS_RUNNING = 'running';
-
-export const DATA = 'data';
-export const MODEL = 'model';
-export const EVALUATE = 'evaluate';
-export const MERGE = 'merge';
-export const MODELTOSAVE = 'modeltosave';
-export const ORIGINDATA = 'origindata';
+export const enum OutputType {
+  Data = 'data',
+  Model = 'model',
+  Evaluate = 'evaluate',
+  Merge = 'merge',
+  ModelToSave = 'modeltosave',
+  OriginData = 'origindata',
+  NotSet = 'not set'
+}
