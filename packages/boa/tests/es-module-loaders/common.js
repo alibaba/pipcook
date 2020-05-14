0a142935396ec089244b4585ef26a1a8dfadff48
rickyes <ives199511@gmail.com>
2020-05-14 18:12:09 +0800

    boa: support esm

---
+++ packages/boa/tests/es-module-loaders/common.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-process-exit */
+
+'use strict';
+
+const path = require('path');
+const { test } = require('tap');
+const { spawnSync } = require('child_process');
+
+function getAbsolutePath(relativePath) {
+  return path.join(__dirname, relativePath);
+}
+
+const FLAG = '--experimental-loader';
+const PATH_ESM_LOADER = getAbsolutePath('../../esm/loader.mjs');
+
+// See https://github.com/nodejs/node/pull/29796
+if (process.version < 'v12.11.1') {
+  console.log(`1..0 # Skipped: Current nodejs version: ${
+    process.version} does not support \`--experimental-loader\``);
+  process.exit(0);
+}
+
+function check(t, appPath) {
+  const options = { encoding: 'utf8', stdio: 'inherit' };
+  const args = ['--no-warnings', FLAG, PATH_ESM_LOADER];
+  // See https://github.com/nodejs/node/pull/31974
+  if (process.version < 'v14.0.0') {
+    args.push('--experimental-modules');
+  }
+  args.push(getAbsolutePath(appPath));
+  // Running with tap causes errors
+  // See https://github.com/tapjs/node-tap/issues/673
+  // 
+  // The nyc 14 conflicts with the node `--experimental-loader` design,
+  // which currently uses nyc 15 and tap 14 in combination with a skip error.
+  const result = spawnSync(process.execPath, args, options);
+  t.strictEqual(result.signal, null);
+  t.strictEqual(result.status, 0);
+  t.end();
+}
+
+test('python stdlib', t => check(t, './py/test-esm-loader-stdlib.mjs'));
+
+test('python thirdparty', t => check(t, './py/test-esm-loader-thirdparty.mjs'));
+
+test('python custom', t => check(t, './py/test-esm-loader-custom.mjs'));
+
+test('javascript thirdparty', t => check(t, './js/test-esm-loader-thirdparty.mjs'));
+
+test('javascript custom', t => check(t, './js/test-esm-loader-custom.mjs'));
---
+++ packages/boa/tests/es-module-loaders/js/test-esm-loader-math.mjs
@@ -0,0 +1,8 @@
+const math = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b
+};
+
+export const add = math.add;
+export const subtract = math.subtract;
+export default math;
