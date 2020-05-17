5b92e9ef78d6f7863dca23392d9ae341778defba
queyue.crk <queyue.crk@alibaba-inc.com>
2020-05-17 18:44:31 +0800

    fix

--- packages/cli/src/request.ts
+++ packages/cli/src/request.ts
@@ -7,14 +7,14 @@ export type RequestParams = Record<string, any>;
 
 export type ResponseParams = Record<string, any>;
 
-const createGeneralRqeuest = (agent: Function) => async (...args: any[]) => {
+const createGeneralRequest = (agent: Function) => async (...args: any[]) => {
   try {
     let response = await agent(...args);
     if (response.data.status === true) {
       return response.data.data;
     }
   } catch (err) {
-    if (err?.response.data.message) {
+    if (err?.response?.data?.message) {
       spinner.fail(err.response.data.message);
       process.exit();
     } else {
@@ -22,7 +22,7 @@ const createGeneralRqeuest = (agent: Function) => async (...args: any[]) => {
     }
   }
 };
-export const get = async (host: string, params?: RequestParams) => createGeneralRqeuest(axios.get)(host, params);
-export const post = async (host: string, body?: RequestParams, params?: RequestParams) => createGeneralRqeuest(axios.post)(host, body, params);
-export const put = async (host: string, body?: RequestParams, params?: RequestParams) => createGeneralRqeuest(axios.put)(host, body, params);
-export const remove = async (host: string) => createGeneralRqeuest(axios.delete)(host);
+export const get = async (host: string, params?: RequestParams) => createGeneralRequest(axios.get)(host, params);
+export const post = async (host: string, body?: RequestParams, params?: RequestParams) => createGeneralRequest(axios.post)(host, body, params);
+export const put = async (host: string, body?: RequestParams, params?: RequestParams) => createGeneralRequest(axios.put)(host, body, params);
+export const remove = async (host: string) => createGeneralRequest(axios.delete)(host);
