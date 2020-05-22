9899eb778f1469f72b17806d121ea469d958fb14
yorkie <yorkiefixer@gmail.com>
2020-05-23 02:18:07 +0800

    remove unused code

--- packages/daemon/src/runner/config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from '@pipcook/pipcook-core';
-
-const config: Config = {
-  // See https://docs.npmjs.com/misc/scripts#packagejson-vars
-  version: process.env.npm_package_version
-};
-
-export default config;
