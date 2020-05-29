c9e096c2bc92680ad99eb1abe276d2bc33b709fd
FeelyChau <feely@outlook.com>
2020-05-29 17:24:48 +0800

    remove changes

--- packages/boa/tools/utils.js
+++ packages/boa/tools/utils.js
@@ -5,28 +5,13 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 const { BOA_CONDA_PREFIX, BOA_CONDA_INDEX } = process.env;
+const CONDA_INSTALL_DIR = path.join(__dirname, '../.CONDA_INSTALL_DIR');
 
-exports.CONDA_INSTALL_DIR = path.join(__dirname, '../.CONDA_INSTALL_DIR');
 exports.run = (...args) => execSync.call(null, args.join(' '), { stdio: 'inherit' });
 exports.PLATFORM = os.platform();
 exports.ARCH = os.arch();
 
-let condaDownloadName = 'Miniconda3-latest';
-if (this.PLATFORM === 'linux') {
-  condaDownloadName += '-Linux';
-} else if (this.PLATFORM === 'darwin') {
-  condaDownloadName += '-MacOSX';
-} else {
-  throw new TypeError(`No support for your platform ${this.PLATFORM}`);
-}
-if (this.ARCH === 'x64') {
-  condaDownloadName += '-x86_64';
-} else if (this.PLATFORM !== 'darwin') {
-  condaDownloadName += '-x86';
-}
-exports.CONDA_DOWNLOAD_NAME = `${condaDownloadName}.sh`;
-
-exports.getCondaPath = () => fs.readFileSync(this.CONDA_INSTALL_DIR, 'utf8');
+exports.getCondaPath = () => fs.readFileSync(CONDA_INSTALL_DIR, 'utf8');
 exports.printCondaPath = () => console.log(this.getCondaPath());
 exports.initAndGetCondaPath = () => {
   let condaPath;
@@ -37,7 +22,7 @@ exports.initAndGetCondaPath = () => {
   } else {
     condaPath = path.join(BOA_CONDA_PREFIX, '.miniconda');
   }
-  fs.writeFileSync(this.CONDA_INSTALL_DIR, condaPath, 'utf8');
+  fs.writeFileSync(CONDA_INSTALL_DIR, condaPath, 'utf8');
   return condaPath;
 };
 
