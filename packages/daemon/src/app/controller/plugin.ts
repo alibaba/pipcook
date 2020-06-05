9fe5671f3893dd71be8a984b2675b3b6f4bdfbf3
FeelyChau <feely@outlook.com>
2020-06-05 14:16:40 +0800

    daemon, cli: add plugin command installFromConfig

--- packages/daemon/src/app/controller/plugin.ts
+++ packages/daemon/src/app/controller/plugin.ts
@@ -2,8 +2,10 @@ import { Context, controller, inject, provide, get } from 'midway';
 import { successRes } from '../../utils/response';
 import { PluginManager } from '../../service/plugin';
 import ServerSentEmitter from '../../utils/emitter';
+import { parseConfig } from '../../runner/helper';
 import Debug from 'debug';
 const debug = Debug('daemon.app.plugin');
+import { constants } from '@pipcook/pipcook-core';
 
 @provide()
 @controller('/plugin')
@@ -27,6 +29,35 @@ export class PluginController {
       debug(`installing ${name}.`);
       await this.pluginManager.install(pkg, pyIndex);
       sse.emit('installed', pkg);
+    } catch (err) {
+      sse.emit('error', err?.message);
+    } finally {
+      sse.finish();
+    }
+  }
+
+  @get('/installFromConfig')
+  public async installFromConfig() {
+    const { config, pyIndex } = this.ctx.query;
+    const configObj = await parseConfig(config);
+    console.log(config, configObj);
+    const sse = new ServerSentEmitter(this.ctx);
+    try {
+      for (const i in constants.PLUGINS) {
+        const plugin = constants.PLUGINS[i];
+        if (!configObj[plugin]) {
+          continue;
+        }
+        console.log(`constants.PLUGINS.forEach ${plugin} ${configObj[plugin]}`);
+        debug(`start installation: ${plugin}`);
+        const pkg = await this.pluginManager.fetch(configObj[plugin]);
+        sse.emit('info', pkg);
+
+        debug(`installing ${configObj[plugin]}.`);
+        await this.pluginManager.install(pkg, pyIndex);
+        sse.emit('installed', pkg);
+      }
+      sse.emit('finished', configObj);
     } catch (err) {
       sse.emit('error', err?.message);
     } finally {
