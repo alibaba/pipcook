fad67b8a238d7546600d28cbdafce2fafc4c52c2
queyue.crk <queyue.crk@alibaba-inc.com>
2020-06-14 09:55:04 +0800

    add mobilenet

---
+++ packages/plugins/model-define/tensorflow-mobilenet-model-define/src/index.ts
@@ -0,0 +1,111 @@
+/**
+ * @file This plugin is used to load the mobileNet for image classification. The input layer is modified to fit into any shape of input.
+ * The final layer is changed to a softmax layer to match the output shape
+ */
+
+import { ModelDefineType, ImageDataset, ImageSample, ModelDefineArgsType, UniModel } from '@pipcook/pipcook-core';
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const boa = require('@pipcook/boa');
+const tf = boa.import('tensorflow');
+const { Adam } = boa.import('tensorflow.keras.optimizers');
+const { MobileNetV2 } = boa.import('tensorflow.keras.applications');
+const { GlobalAveragePooling2D, Dropout, Dense } = boa.import('tensorflow.keras.layers');
+const { Model } = boa.import('tensorflow.keras.models');
+
+/** @ignore
+ * assertion test
+ * @param data
+ */
+const assertionTest = (data: ImageDataset) => {
+  assert.ok(data.metadata.feature, 'Image feature is missing');
+  assert.ok(data.metadata.feature.shape.length === 3, 'The size of an image must be 3d');
+};
+
+/**
+ *  main function of the operator: load the mobilenet model
+ * @param data sample data
+ */
+const mobilenetDefine: ModelDefineType = async (data: ImageDataset, args: ModelDefineArgsType): Promise<UniModel> => {
+  let {
+    loss = 'categorical_crossentropy',
+    metrics = [ 'accuracy' ],
+    learningRate = 0.001,
+    decay = 0.05,
+    recoverPath,
+    labelMap,
+    freeze = false
+  } = args;
+
+  let inputShape: number[];
+  let outputShape: number;
+
+  if (!recoverPath) {
+    assertionTest(data);
+    inputShape = data.metadata.feature.shape;
+    outputShape = Object.keys(data.metadata.labelMap).length;
+    labelMap = data.metadata.labelMap;
+  } else {
+    const log = JSON.parse(fs.readFileSync(path.join(recoverPath, 'log.json'), 'utf8'));
+    labelMap = log.metadata.labelMap;
+    outputShape = Object.keys(labelMap).length;
+  }
+
+  let model: any;
+  model = MobileNetV2(boa.kwargs({
+    include_top: false,
+    weights: 'imagenet',
+    input_shape: inputShape
+  }));
+
+  let output = model.output;
+  output = GlobalAveragePooling2D()(output);
+  output = Dense(1024, boa.kwargs({
+    activation: 'relu'
+  }))(output);
+  output = Dropout(0.5)(output);
+
+  const outputs = Dense(outputShape, boa.kwargs({
+    activation: 'softmax'
+  }))(output);
+  model = Model(boa.kwargs({
+    inputs: model.input,
+    outputs: outputs
+  }));
+
+  if (freeze) {
+    for (let layer of model.layers.slice(0, -10)) {
+      layer.trainable = false;
+    }
+  }
+
+  if (recoverPath) {
+    model.load_weights(recoverPath);
+  }
+
+  model.compile(boa.kwargs({
+    optimizer: Adam(boa.kwargs({
+      lr: learningRate,
+      decay
+    })),
+    loss: loss,
+    metrics: metrics
+  }));
+
+  const result: UniModel = {
+    model,
+    metrics: metrics,
+    predict: async function (inputData: ImageSample) {
+      let image = tf.io.read_file(inputData.data);
+      image = tf.image.decode_jpeg(image, boa.kwargs({
+        channels: 3
+      }));
+      return this.model.predict(image).toString();
+    }
+  };
+  return result;
+};
+
+export default mobilenetDefine;
