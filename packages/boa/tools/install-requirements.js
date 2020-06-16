851458d8eb74ae210ea3944e3c1834b36845c3d7
Ruikun Cao <queyue.crk@alibaba-inc.com>
2020-06-16 19:51:31 +0800

    model: add python version's mobilenet (#239)

--- packages/boa/tools/install-requirements.js
+++ packages/boa/tools/install-requirements.js
@@ -3,4 +3,4 @@
 const path = require('path');
 const { pip } = require('./utils');
 
-pip('install', `-r ${path.join(__dirname, '../requirements.txt')}`);
+pip('install', `-r ${path.join(__dirname, '../requirements.txt')}`, `--default-timeout=1000`);
