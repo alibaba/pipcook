56d29cc361f05c3d56c518a83d867069d21d4831
Yorkie Liu <yorkiefixer@gmail.com>
2020-06-18 19:48:32 +0800

    boa: fix the install script python (#250)

--- packages/boa/lib/index.js
+++ packages/boa/lib/index.js
@@ -10,6 +10,15 @@ const DelegatorLoader = require('./delegator-loader');
 // internal symbols
 const IterIdxForSeqSymbol = Symbol('The iteration index for sequence');
 
+// read the conda path from the .CONDA_INSTALL_DIR
+// eslint-disable-next-line no-sync
+const condaPath = fs.readFileSync(path.join(__dirname, '../.CONDA_INSTALL_DIR'), 'utf8');
+// eslint-disable-next-line no-process-env
+if (!process.env.PYTHONHOME) {
+  // eslint-disable-next-line no-process-env
+  process.env.PYTHONHOME = condaPath;
+}
+
 // create the global-scoped instance
 let pyInst = global.__pipcook_boa_pyinst__;
 if (pyInst == null) {
@@ -41,10 +50,6 @@ function getTypeInfo(T) {
 }
 
 function setenv(externalSearchPath) {
-  // read the conda path from the .CONDA_INSTALL_DIR
-  // eslint-disable-next-line no-sync
-  const condaPath = fs.readFileSync(
-    path.join(__dirname, '../.CONDA_INSTALL_DIR'), 'utf8');
   const sys = pyInst.import('sys');
   if (!defaultSysPath || !defaultSysPath.length) {
     defaultSysPath = vm.runInThisContext(sys.__getattr__('path').toString()) || [];
