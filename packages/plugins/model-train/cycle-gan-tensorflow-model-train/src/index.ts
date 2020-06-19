2ee6657ac3867306f92549bf5af1f7711be1501e
FeelyChau <feely@outlook.com>
2020-06-19 13:52:03 +0800

    fix

--- packages/plugins/model-train/cycle-gan-tensorflow-model-train/src/index.ts
+++ packages/plugins/model-train/cycle-gan-tensorflow-model-train/src/index.ts
@@ -8,13 +8,16 @@ sys.path.insert(0, path.join(__dirname, '..'));
 const { ImageGenerator } = boa.import('image_loader');
 
 const cycleGANModelTrain: ModelTrainType = async (data: ImageDataset, model: UniModel, args: ModelTrainArgsType): Promise<UniModel> => {
+  const { 
+    resize = [ 143, 143 ],
+    crop = [ 128, 128 ]
+  } = args;
   const { trainLoader } = data;
-  console.log('len', await trainLoader.len())
   const aList = [];
   const bList = [];
-  for(let i = 0; i < await trainLoader.len(); ++i) {
+  for (let i = 0; i < await trainLoader.len(); ++i) {
     const image = trainLoader.getItem(i);
-    switch((await image).label.name.toLowerCase()) {
+    switch ((await image).label.name.toLowerCase()) {
       case 'a':
         aList.push((await image).data);
         break;
@@ -29,16 +32,16 @@ const cycleGANModelTrain: ModelTrainType = async (data: ImageDataset, model: Uni
 
   const IG_A = new ImageGenerator(
     boa.kwargs({
-      fileList: aList, 
-      resize: tuple([int(143), int(143)]),
-      crop: tuple([int(128), int(128)])
+      fileList: aList,
+      resize: tuple([ int(resize[0]), int(resize[1]) ]),
+      crop: tuple([ int(crop[0]), int(crop[1]) ])
     })
   );
   const IG_B = new ImageGenerator(
     boa.kwargs({
       fileList: bList,
-      resize: tuple([int(143), int(143)]),
-      crop: tuple([int(128), int(128)])
+      resize: tuple([ int(resize[0]), int(resize[1]) ]),
+      crop: tuple([ int(crop[0]), int(crop[1]) ])
     })
   );
   model.model.fit(IG_A, IG_B);
