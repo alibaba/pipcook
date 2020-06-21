7243f8eb6eec30bfe18be8c3a0a54a2c3733c564
tao <marshaodwcc@gmail.com>
2020-06-21 17:31:48 +0800

    added and changed image process in data-process-plugin

--- packages/plugins/data-process/image-data-process/src/index.ts
+++ packages/plugins/data-process/image-data-process/src/index.ts
@@ -1,29 +1,33 @@
-import { DataProcessType, Metadata, ArgsType, ImageSample } from '@pipcook/pipcook-core';
-
-import Jimp from 'jimp';
+import { DataProcessType, Metadata, ArgsType, ImageSample, ImageProcessor } from '@pipcook/pipcook-core';
 
 /**
  * this is the data process plugin to process pasvoc format data. It supports resize the image and normalize the image
  * @param resize =[256, 256][optional] resize all images to same size
- * @param normalize =false[optional] if normalize all images to have values between [0, 1]
+ * @param minMaxNormalize =true[optional] if normalize all images to have values between [0, 1]
+ * @param normalize =false[optional] if standardize all image by std and mean
  */
 const imageDataProcess: DataProcessType = async (data: ImageSample, metadata: Metadata, args: ArgsType): Promise<void> => {
   const {
     resize = [ 256, 256 ],
-    normalize = false
+    normalize = false,
+    minMaxNormalize = true
   } = args;
 
   try {
-    let image = await Jimp.read(data.data);
-    image = image.resize(resize[0], resize[1]);
-    if (normalize) {
-      image = image.normalize();
+    const processor = new ImageProcessor(data.data);
+    if(minMaxNormalize) {
+      processor.normalize();
     }
-    await image.writeAsync(data.data);
+    if(resize) {
+      processor.resize(resize);
+    }
+    if(normalize) {
+      processor.standardize(normalize.std, normalize.mean);
+    }
+    await processor.excute().save(data.data);
   } catch (err) {
     console.error(`processing ${data.data} failed with ${err?.stack}`);
   }
-
   metadata.feature = {
     shape: [ resize[0], resize[1], 3 ]
   };
