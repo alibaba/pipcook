3de106d10483535b5314f970605669b81e12bd72
queyue.crk <queyue.crk@alibaba-inc.com>
2020-06-22 10:06:01 +0800

    add shuffle to dataset

--- packages/core/src/utils/public.ts
+++ packages/core/src/utils/public.ts
@@ -290,3 +290,15 @@ export function getOsInfo() {
     });
   });
 }
+
+/**
+ * shuffle array
+ * @param array
+ */
+export function shuffle(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [ array[i], array[j] ] = [ array[j], array[i] ];
+  }
+  return array;
+}
