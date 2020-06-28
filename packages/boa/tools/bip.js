fe5454058a682c616dcf1afa18bce00bc964f8ba
Yorkie Liu <yorkiefixer@gmail.com>
2020-06-28 15:40:16 +0800

    Update bip.js

--- packages/boa/tools/bip.js
+++ packages/boa/tools/bip.js
@@ -5,7 +5,7 @@ const { run, getCondaPath } = require('./utils');
 
 const args = process.argv.slice(2).join(' ');
 const condaPath = getCondaPath();
-if (!fs.existsSync) {
+if (!fs.existsSync(condaPath)) {
   throw new TypeError('conda env is not initialized.');
 }
 
