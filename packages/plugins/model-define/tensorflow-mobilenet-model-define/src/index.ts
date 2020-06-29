3a31c3eecec36261d04446c582e9a3619685bd79
queyue.crk <queyue.crk@alibaba-inc.com>
2020-06-29 15:42:27 +0800

    bump version: 1.0.0

--- packages/plugins/model-define/tensorflow-mobilenet-model-define/src/index.ts
+++ packages/plugins/model-define/tensorflow-mobilenet-model-define/src/index.ts
@@ -116,7 +116,7 @@ const mobilenetDefine: ModelDefineType = async (data: ImageDataset, args: ModelD
         channels: 3
       }));
       const shape = tf.shape(image).numpy();
-      return this.model.predict(tf.reshape(image, [ 1 ].concat(shape.slice(0, 3)))).toString();
+      return this.model.predict(tf.reshape(image, [ 1 ].concat(...shape.slice(0, 3)))).toString();
     }
   };
   return result;
