5eaacd7212d5da62faa20ded39b3ea6f3bff7612
FeelyChau <feely@outlook.com>
2020-07-02 11:40:52 +0800

    fixup

--- packages/boa/tools/check-dependence.js
+++ packages/boa/tools/check-dependence.js
@@ -1,23 +1,22 @@
 'use strict';
 const { run, PLATFORM } = require('./utils');
-const version = process.version.split('.')[0];
 
-if (parseInt(version.substr(1)) < 12) {
+const majorVersion = process.version.split('.')[0];
+if (parseInt(majorVersion.substr(1)) < 12) {
   throw new TypeError(`require Node.js >= v12.0.0, but ${process.version} found`);
 }
 
-let cmds = [ 'rm', 'make', 'tar' ];
+const cmds = [ 'rm', 'make', 'tar' ];
 
 if (PLATFORM === 'linux') {
-  cmds = cmds.concat([ 'wget' ]);
+  cmds.push('wget');
 } else if (PLATFORM === 'darwin') {
-  cmds = cmds.concat([ 'curl' ]);
+  cmds.push('curl');
 } else {
   throw new TypeError(`No support for your platform ${PLATFORM}`);
 }
 
 cmds.forEach(cmd => {
-  console.log('checking:');
   try {
     run(`command -v ${cmd}`);
   } catch (err) {
