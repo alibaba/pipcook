f3175be4f386e1a6702ad2d36a1e66028e85a976
FeelyChau <feely@outlook.com>
2020-07-09 00:19:46 +0800

    fixup

---
+++ packages/sdk/src/interface.ts
@@ -0,0 +1,43 @@
+
+export interface JobModel {
+  id: string;
+  pipelineId: string;
+  specVersion: string;
+  metadata: number;
+  evaluateMap: string;
+  evaluatePass: boolean;
+  currentIndex: number;
+  error: string;
+  endTime: number;
+  status: number;
+  dataset: string;
+}
+
+export interface PipelineModel {
+  id: string;
+  name: string;
+  dataCollect: string;
+  dataCollectParams: string;
+  dataAccess: string;
+  dataAccessParams: string;
+  dataProcess: string;
+  dataProcessParams: string;
+  modelDefine: string;
+  modelDefineParams: string;
+  modelLoad: string;
+  modelLoadParams: string;
+  modelTrain: string;
+  modelTrainParams: string;
+  modelEvaluate: string;
+  modelEvaluateParams: string;
+}
+
+export interface JobRunOption {
+  pipelineId: string;
+  timeout: number;
+  tuna: boolean;
+}
+
+export interface PipelineInstallOption {
+  tuna: boolean;
+}
