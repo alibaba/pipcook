0c171bf6be5a811f05742fa6870f2b13e1d855ff
WenheLI <eric.lwh@alibaba-inc.com>
2020-07-23 13:38:11 +0800

    Change identation to 2

--- packages/cli/assets/pluginPackage/src/modelDefine.ts
+++ packages/cli/assets/pluginPackage/src/modelDefine.ts
@@ -1,23 +1,23 @@
 import {
-    ModelDefineType,
-    UniDataset,
-    ModelDefineArgsType,
-    UniModel,
-    Sample
+  ModelDefineType,
+  UniDataset,
+  ModelDefineArgsType,
+  UniModel,
+  Sample
 } from '@pipcook/pipcook-core';
 
 const templateModelDefine: ModelDefineType = async (data: UniDataset, args: ModelDefineArgsType): Promise <UniModel> => {
 
-    let model = {}; // Some model defination
+  let model = {}; // Some model defination
 
-    const retModelDefine: UniModel = {
-        model,
-        predict: async function (input: Sample) {
-            const pred = this.model.predict(input.data);
-            return pred.toString();
-        }
-    };
-    return retModelDefine;
+  const retModelDefine: UniModel = {
+    model,
+    predict: async function (input: Sample) {
+      const pred = this.model.predict(input.data);
+      return pred.toString();
+    }
+  };
+  return retModelDefine;
 };
 
 export default templateModelDefine;
