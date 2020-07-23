3aefa82b10c543df63b1571fb50bb4fbfbecb6ca
WenheLI <wl1508@nyu.edu>
2020-07-23 10:15:10 +0800

    fix: add modelDefine template

---
+++ packages/cli/assets/pluginPackage/src/modelDefine.ts
@@ -0,0 +1,23 @@
+import {
+    ModelDefineType,
+    UniDataset,
+    ModelDefineArgsType,
+    UniModel,
+    Sample
+} from "@pipcook/pipcook-core";
+
+const templateModelDefine: ModelDefineType = async (data: UniDataset, args: ModelDefineArgsType): Promise <UniModel> => {
+
+    let model = {}; // Some model defination
+
+    const retModelDefine: UniModel = {
+        model,
+        predict: async function (input: Sample) {
+            const pred = this.model.predict(input.data);
+            return pred.toString();
+        }
+    };
+    return retModelDefine;
+};
+
+export default templateModelDefine;
