19a86b57a15a3550407c10973cec23c39598100c
WenheLI <eric.lwh@alibaba-inc.com>
2020-08-07 13:04:54 +0800

    daemon: add migration support (#397)
    * feat: add migration support
    
    Co-authored-by: Yorkie Liu <yorkiefixer@gmail.com>

---
+++ packages/daemon/src/migrations/20200803063219-addDatasetProcess.ts
@@ -0,0 +1,24 @@
+'use strict';
+import { QueryInterface, DataTypes } from 'sequelize';
+
+export default {
+  up: async (queryInterface: QueryInterface) => {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.addColumn('pipelines', 'datasetProcess', {
+          type: DataTypes.STRING
+        }, { transaction: t }),
+        queryInterface.addColumn('pipelines', 'datasetProcessParams', {
+          type: DataTypes.STRING
+        }, { transaction: t })
+      ]);
+    });
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return queryInterface.sequelize.transaction(async t => {
+        await queryInterface.removeColumn('pipelines', 'datasetProcess', { transaction: t });
+        await queryInterface.removeColumn('pipelines', 'datasetProcessParams', { transaction: t });
+    });
+  }
+};
