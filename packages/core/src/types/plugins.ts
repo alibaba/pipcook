95f5a7cb09d121f458656100dcbdbf68e9f410cb
WenheLI <eric.lwh@alibaba-inc.com>
2020-08-08 11:06:15 +0800

    plugin: add GlobalDataProcess Plugin (#381)
    * feat: add GlobalDataProcess plugin
    
    * fix: fix lint issue
    
    * fix: fix lint issue & rename datasetProcess
    
    * add
    
    * fix: add inline comments
    
    * doc: fix command typo (#415)
    
    * daemon: add migration support (#397)
    
    * feat: add migration support
    
    Co-authored-by: Yorkie Liu <yorkiefixer@gmail.com>
    
    * costa: add python runnable for machine-learning developers (#388)
    
    Co-authored-by: Eder <jzx19951028yyjy@163.com>
    Co-authored-by: Yorkie Liu <yorkiefixer@gmail.com>

--- packages/core/src/types/plugins.ts
+++ packages/core/src/types/plugins.ts
@@ -1,4 +1,3 @@
-
 import { UniDataset, Sample, Metadata } from './data/common';
 import { UniModel } from './model';
 import { EvaluateResult } from './other';
@@ -9,7 +8,7 @@ interface InsertParams {
   dataDir: string;
 }
 
-export type PluginTypeI = 'dataCollect' | 'dataAccess' | 'dataProcess' | 'modelLoad' | 'modelDefine' |'modelTrain' | 'modelEvaluate';
+export type PluginTypeI = 'dataCollect' | 'dataAccess' | 'dataProcess' | 'datasetProcess' | 'modelLoad' | 'modelDefine' | 'modelTrain' | 'modelEvaluate';
 
 /**
  * The base type which represents the `Record` from pipeline config file.
@@ -125,6 +124,31 @@ export interface DataAccessType extends PipcookPlugin {
  */
 export interface DataProcessType extends PipcookPlugin {
   (data: Sample, metadata: Metadata, args: ArgsType): Promise<void>;
+}
+
+/**
+ * Similar to `DataProcessType`, but this type targets on the whole dataset rather than a sample.
+ * This plugin will be convenient when it comes to process data that requires information from the whole dataset. I.E. corpus construction, average data among the dataset ...
+ *
+ * @example
+ *
+ * ```js
+ * const getCorpus = async (dataset: UniDataset, metadata: Metadata, args?: ArgsType): Promise<void> => {
+ *  const corpus: Set<string> = new Set();
+ *  for (const data of dataset) {
+ *    for (const word of (data.data.split(" "))) {
+ *      corpus.add(word);
+ *    }
+ *  }
+ *  metadata.corpus = corpus;
+ * }
+ * ```
+ *
+ * @param dataset The dataset of which you loaded in `dataCollect` & `dataAccess`
+ * @param args The arguments from pipeline config file.
+ */
+export interface DatasetProcessType extends PipcookPlugin {
+  (dataset: UniDataset, args: ArgsType): Promise<void>;
 }
 
 /**
