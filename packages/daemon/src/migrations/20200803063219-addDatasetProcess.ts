59ebbd229532cc427a8f0ef7a4a13fe204dd84a9
WenheLI <eric.lwh@alibaba-inc.com>
2020-08-11 17:40:11 +0800

    daemon: add fix for db migration (#426)
    * fix: add fix for db migration
    
    * add EOF
    
    * Update packages/daemon/bootstrap.js
    
    Co-authored-by: Yorkie Liu <yorkiefixer@gmail.com>
    
    * change tables => columns
    
    Co-authored-by: Yorkie Liu <yorkiefixer@gmail.com>

--- packages/daemon/src/migrations/20200803063219-addDatasetProcess.ts
+++ packages/daemon/src/migrations/20200803063219-addDatasetProcess.ts
@@ -6,11 +6,12 @@ export default {
     return queryInterface.sequelize.transaction(async t => {
       const tableNames = await queryInterface.showAllTables();
       if (tableNames.indexOf('pipelines') >= 0) {
+        const columns = await queryInterface.describeTable('pipelines');
         return Promise.all([
-          queryInterface.addColumn('pipelines', 'datasetProcess', {
+          !columns['datasetProcess'] && queryInterface.addColumn('pipelines', 'datasetProcess', {
             type: DataTypes.STRING
           }, { transaction: t }),
-          queryInterface.addColumn('pipelines', 'datasetProcessParams', {
+          !columns['datasetProcess'] && queryInterface.addColumn('pipelines', 'datasetProcessParams', {
             type: DataTypes.STRING
           }, { transaction: t })
         ]);
@@ -22,8 +23,9 @@ export default {
     return queryInterface.sequelize.transaction(async t => {
       const tableNames = await queryInterface.showAllTables();
       if (tableNames.indexOf('pipelines') >= 0) {
-        await queryInterface.removeColumn('pipelines', 'datasetProcess', { transaction: t });
-        await queryInterface.removeColumn('pipelines', 'datasetProcessParams', { transaction: t });
+        const columns = await queryInterface.describeTable('pipelines');
+        columns['datasetProcess'] && await queryInterface.removeColumn('pipelines', 'datasetProcess', { transaction: t });
+        columns['datasetProcess'] && await queryInterface.removeColumn('pipelines', 'datasetProcessParams', { transaction: t });
       }
     });
   }
