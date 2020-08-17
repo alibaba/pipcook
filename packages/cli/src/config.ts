3d8aab0c7ecab0104d8ef64b3bc569e9672a7567
WenheLI <wl1508@nyu.edu>
2020-08-17 20:40:29 +0800

    add

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -7,7 +7,7 @@ export const dependencies: string[] = [
 
 export const isLocal = process.env.NODE_ENV === 'local';
 export const daemonPackage = isLocal ? path.join(__dirname, '../../daemon') : '@pipcook/daemon';
-export const boardPackage = isLocal ? path.join(__dirname, '../../pipboard') : '@pipcook/pipboard';
+export const boardPackage = '@pipcook/pipboard';
 
 export const pipcookLogName = 'pipcook-output';
 export const optionalNpmClients: string[] = [ 'npm', 'cnpm' ];
