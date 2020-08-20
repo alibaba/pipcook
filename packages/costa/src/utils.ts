8d6d989b96e0adf4eb882e7f704aa1136f409b48
feely <feely@outlook.com>
2020-08-20 12:34:24 +0800

    costa: install Node and Python packages in parallel (#477)

--- packages/costa/src/utils.ts
+++ packages/costa/src/utils.ts
@@ -1,6 +1,7 @@
 export interface LogStdio {
   stdout: NodeJS.WritableStream;
   stderr: NodeJS.WritableStream;
+  prefix?: string;
 }
 
 /**
@@ -8,13 +9,24 @@ export interface LogStdio {
  * Readable.pipi() will close the target pipe when end, we should ignore the end event.
  * @param readable child process stdout/stderr
  * @param writable the log stream
+ * @param prefix the log prefix
  */
-export function pipeLog(readable: NodeJS.ReadableStream, writable: NodeJS.WritableStream): void {
+export function pipeLog(readable: NodeJS.ReadableStream, writable: NodeJS.WritableStream, prefix?: string): void {
+  let buffer = '';
   readable.on('error', (err) => {
     writable.emit('error', err);
   });
-  readable.on('data', async (data) => {
-    writable.write(data);
+  readable.on('data', (data) => {
+    if (prefix) {
+      buffer += data.toString();
+      const list = buffer.split(/\n|\r/);
+      buffer = list.pop();
+      list.forEach((log) => {
+        writable.write(`${prefix}: ${log}\n`);
+      });
+    } else {
+      writable.write(data);
+    }
   });
 }
 
