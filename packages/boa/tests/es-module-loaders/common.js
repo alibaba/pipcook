d96a9c95f6604c3c351d0fb95cceede68e004c1f
NHibiki <18514672+NHibiki@users.noreply.github.com>
2020-08-21 15:50:05 +0800

    ci: fix module loader ci

--- packages/boa/tests/es-module-loaders/common.js
+++ packages/boa/tests/es-module-loaders/common.js
@@ -13,7 +13,7 @@ function getAbsolutePath(relativePath) {
 const FLAG = '--experimental-loader';
 const PATH_ESM_LOADER = getAbsolutePath('../../esm/loader.mjs');
 
-const [major, minor] = process.version.replace('v', '').split('.');
+const [major] = process.version.replace('v', '').split('.');
 if (major <= '12') {
   // See https://github.com/nodejs/node/pull/29796
   console.log(`1..0 # Skipped: Current nodejs version: ${
