9e9caca9410b67fe17f882f5e297fde19aab1788
yorkie <yorkiefixer@gmail.com>
2020-08-23 02:18:34 +0800

    daemon: add versions and config api

--- packages/daemon/src/app/controller/index.ts
+++ packages/daemon/src/app/controller/index.ts
@@ -1,4 +1,7 @@
 import { Context, controller, inject, provide, get } from 'midway';
+import { readJSON } from 'fs-extra';
+import { join } from 'path';
+import { constants } from '@pipcook/pipcook-core';
 
 @provide()
 @controller('/')
@@ -9,5 +12,21 @@ export class IndexController {
   @get('/')
   public async index() {
     this.ctx.redirect('/index.html');
+  }
+
+  @get('/versions')
+  public async versions() {
+    const daemonPkg = await readJSON(join(constants.PIPCOOK_DAEMON_SRC, 'package.json'));
+    this.ctx.success({
+      versions: {
+        daemon: daemonPkg.version
+      }
+    });
+  }
+
+  @get('/config')
+  public async config() {
+    const data = await readJSON(constants.PIPCOOK_DAEMON_CONFIG);
+    this.ctx.success(data);
   }
 }
