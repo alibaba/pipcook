b14474aa506f3e1efee9d500ba21915e2b8c73f7
FeelyChau <feely@outlook.com>
2020-08-26 21:38:45 +0800

    fixup

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,4 @@ lerna run test \
   --scope @pipcook/pipcook-core \
   --scope @pipcook/boa \
   --scope @pipcook/costa \
-  --scope @pipcook/daemon \
-  --scope @pipcook/sdk
-
+  --scope @pipcook/daemon
