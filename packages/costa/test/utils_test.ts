8f3c4b2b041be764fd56dd723046199e91636b7e
feely <feely@outlook.com>
2020-08-28 00:10:57 +0800

    costa: remove dependecies when uninstall plugins (#518)

--- packages/costa/test/utils_test.ts
+++ packages/costa/test/utils_test.ts
@@ -5,10 +5,10 @@ import { pipeGracefully } from '../src/utils';
 it('test pipe with finish event', async () => {
   let step = 0;
   const writeFile = path.join(__dirname, 'utils_test.ts_test');
-  const readStream = createReadStream(path.join(__dirname, 'utils_test.ts'));
+  const readStream = createReadStream(path.join(__dirname, 'utils_test.js'));
   const writeStream = createWriteStream(writeFile);
-  readStream.on('end', () => expect(++step).toBe(0));
-  writeStream.on('finish', () => expect(++step).toBe(1));
+  readStream.on('end', () => expect(step++).toBe(0));
+  writeStream.on('finish', () => expect(step++).toBe(1));
   await pipeGracefully(readStream, writeStream);
   expect(step).toBe(2);
   await remove(writeFile);
