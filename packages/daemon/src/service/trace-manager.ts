31299fdee645029b453fab9a31eadbcd785166a7
feely <feely@outlook.com>
2020-09-02 09:38:27 +0800

    daemon: add job status change event (#526)
    * daemon: add job status change event
    
    * unexport LogLevel

---
+++ packages/daemon/src/service/trace-manager.ts
@@ -0,0 +1,263 @@
+import { Transform, TransformCallback } from 'stream';
+import { EventEmitter } from 'events';
+import { open, close, write } from 'fs-extra';
+import { provide, scope, ScopeEnum } from 'midway';
+import { StringDecoder } from 'string_decoder';
+import { generateId, PipelineStatus, PluginTypeI } from '@pipcook/pipcook-core';
+import Debug from 'debug';
+
+const debug = Debug('daemon.service.tracer');
+
+export type TraceType = 'log' | 'job_status';
+
+/**
+ * base pipcook event, defined the fields `type` and `data`
+ */
+export class TraceEvent {
+  data?: any;
+  constructor(public type: TraceType) {}
+}
+
+/**
+ * pipcook event data type for job status change
+ */
+export class JobStatusChangeEvent extends TraceEvent {
+  data: {
+    jobStatus: PipelineStatus;
+    step?: PluginTypeI;
+    stepAction?: 'start' | 'end';
+    queueLength?: number;
+  };
+  constructor(jobStatus: PipelineStatus, step?: PluginTypeI, stepAction?: 'start' | 'end', queueLength?: number) {
+    super('job_status');
+    this.data = { jobStatus, step, stepAction, queueLength };
+  }
+}
+
+type LogLevel = 'info' | 'warn' | 'error';
+/**
+ * pipcook event data type for log
+ */
+export class LogEvent extends TraceEvent {
+  data: {
+    level: LogLevel;
+    data: string;
+  };
+  constructor(level: LogLevel, data: string) {
+    super('log');
+    this.data = { level, data };
+  }
+}
+
+export type PipcookEvent = JobStatusChangeEvent | LogEvent;
+/**
+ * trace handler
+ * it has 2 parts: logger and event handler:
+ * logger:
+ * stdout and stderr, they are streams to pipe the logs to clients
+ * event handler:
+ * pipe the pipcook event to clients
+ */
+export class Tracer {
+  // trace id
+  id: string;
+  // stdout stream for log pipe
+  private stdout: LogPassthrough;
+  // stderr stream for log pipe
+  private stderr: LogPassthrough;
+  // event emitter for pipcook event
+  private dispatcher: EventEmitter;
+
+  constructor(opts?: TraceOptions) {
+    this.id = generateId();
+    this.dispatcher = new EventEmitter();
+    this.stdout = new LogPassthrough(opts?.stdoutFile);
+    this.stderr = new LogPassthrough(opts?.stderrFile);
+  }
+
+  /**
+   * init tracer
+   */
+  async init() {
+    return Promise.all([
+      this.stdout.init(),
+      this.stderr.init()
+    ]);
+  }
+  /**
+   * get the loggers
+   */
+  getLogger(): { stdout: LogPassthrough; stderr: LogPassthrough } {
+    return { stdout: this.stdout, stderr: this.stderr };
+  }
+
+  /**
+   * listen event
+   * @param cb event callback
+   */
+  listen(cb: (data: PipcookEvent) => void): void {
+    // event callback
+    this.dispatcher.on('trace-event', (e) => {
+      cb(e);
+    });
+
+    // log callback
+    const pipeLog = (level: LogLevel, logger: LogPassthrough) => {
+      logger.on('data', data => {
+        cb(new LogEvent(level, data));
+      });
+      logger.on('close', () => this.dispatcher.emit('trace-finished'));
+      logger.on('error', err => {
+        cb(new LogEvent('error', err.message));
+      });
+    };
+    pipeLog('info', this.stdout);
+    pipeLog('warn', this.stderr);
+  }
+
+  /**
+   * dispatch event to client
+   * @param event pipcook event data
+   */
+  dispatch(event: PipcookEvent) {
+    this.dispatcher.emit('trace-event', event);
+  }
+
+  /**
+   * wait for end
+   */
+  async wait() {
+    return new Promise((resolve) => {
+      this.dispatcher.on('trace-finished', resolve);
+    });
+  }
+
+  /**
+   * destory tracer
+   * @param err error if have
+   */
+  destroy(err?: Error) {
+    // TODO(feely): emit the error by tracer not logger
+    this.stderr.finish(err);
+    this.stdout.finish();
+  }
+}
+
+export interface TraceOptions {
+  stdoutFile?: string;
+  stderrFile?: string;
+}
+
+class LogPassthrough extends Transform {
+  decoder = new StringDecoder('utf8');
+  last: string;
+  fd: number;
+  initialized: boolean;
+  filename: string;
+
+  constructor(filename: string) {
+    super({ objectMode: true });
+    this.fd = -1;
+    this.initialized = false;
+    this.filename = filename;
+  }
+  /**
+   * need to be called before transforming, it initializes the log file
+   */
+  async init() {
+    if (this.filename) {
+      this.fd = await open(this.filename, 'w+');
+    }
+    this.initialized = true;
+  }
+
+  _transform(chunk: any, encoding: string, callback: TransformCallback): void {
+    if (!this.initialized) {
+      throw new TypeError('LogPassthrough should be initialized before transforming');
+    }
+    if (this.fd > 0) {
+      write(this.fd, chunk);
+    }
+    if (this.last === undefined) {
+      this.last = '';
+    }
+    this.last += this.decoder.write(chunk);
+    const list = this.last.split(/\n|\r/);
+    this.last = list.pop();
+    list.forEach(line => {
+      this.push(line);
+    });
+    callback();
+  }
+
+  _flush(callback: TransformCallback) {
+    this.last = this.last ? this.last + this.decoder.end() : this.decoder.end();
+    if (this.last) {
+      this.push(this.last);
+    }
+    if (this.fd > 0) {
+      close(this.fd);
+    }
+    callback();
+  }
+
+  writeLine(line: string) {
+    this.write(`${line}\n`);
+  }
+
+  /**
+   * end and destroy the stream.
+   */
+  finish(err?: Error) {
+    this.end();
+    // make sure someone handles the error, otherwise the process will exit
+    if (err && this.listeners('error').length > 0) {
+      this.destroy(err);
+    } else {
+      if (err) {
+        console.error(`unhandled error from log: ${err.message}`);
+      }
+      this.destroy();
+    }
+  }
+}
+
+@scope(ScopeEnum.Singleton)
+@provide('traceManager')
+export class TraceManager {
+  tracerMap = new Map<string, Tracer>();
+
+  /**
+   * create a log object, must call the destroy function to clean it up.
+   */
+  async create(opts?: TraceOptions): Promise<Tracer> {
+    const tracer: Tracer = new Tracer(opts);
+    await tracer.init();
+    this.tracerMap.set(tracer.id, tracer);
+    return tracer;
+  }
+
+  /**
+   * get the tarcer object by trace id.
+   * @param id trace id
+   */
+  get(id: string): Tracer {
+    return this.tracerMap.get(id);
+  }
+
+  /**
+   * clean the tracer object up, emit the end event,
+   * if the trace progress ends with error, it'll be emitted before end event.
+   * @param id trace id
+   * @param err error if have
+   */
+  destroy(id: string, err?: Error) {
+    const tracer = this.tracerMap.get(id);
+    if (tracer) {
+      tracer.destroy(err);
+      return this.tracerMap.delete(id);
+    } else {
+      debug(`tracer ${id} not found for destroy`);
+    }
+  }
+}
---
+++ packages/daemon/test/service/trace-manager.test.ts
@@ -0,0 +1,182 @@
+import { join } from 'path';
+import { app, assert } from 'midway-mock/bootstrap';
+import { TraceManager, Tracer, LogEvent, JobStatusChangeEvent } from '../../src/service/trace-manager';
+import * as fs from 'fs-extra';
+import * as sinon from 'sinon';
+
+const opts = { stdoutFile: join(__dirname, 'stdout.log'), stderrFile: join(__dirname, 'stderr.log')};
+const removeLogs = async () => {
+  return Promise.all([
+    fs.remove(opts.stdoutFile),
+    fs.remove(opts.stderrFile)
+  ]);
+};
+
+describe('test tracer', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  let tracer: Tracer;
+  let tracerWithFile: Tracer;
+  it('#new tracer', async () => {
+    tracer = new Tracer();
+    tracerWithFile = new Tracer(opts);
+  });
+
+  it('#init logger', async () => {
+    sinon.replace(fs, 'open', async (...args: any[]) => {
+      assert.fail('should not open log file');
+    });
+    await tracer.init();
+  });
+  it('#init logger with log files', async () => {
+    await tracer.init();
+    sinon.replace(fs, 'open', async (...args: any[]) => {
+      assert.ok([ opts.stdoutFile, opts.stderrFile ].indexOf(args[0]) >=0);
+      assert.equal(args[1], 'w+');
+      return 1;
+    });
+    await tracerWithFile.init();
+  });
+  it('#listen', async () => {
+    tracer.listen(() => {});
+  });
+  it('#test event: listen/dispatch/wait', async () => {
+    let called = false;
+    tracer.listen((data) => {
+      called = true;
+      assert.equal(data.type, 'log');
+      assert.deepEqual(data.data, { level: 'info', data: 'message' });
+    });
+    process.nextTick(() => {
+      tracer.dispatch(new LogEvent('info', 'message'));
+      tracer.destroy();
+    });
+    await tracer.wait();
+    assert.ok(called);
+    tracer = undefined;
+  });
+  it('#destory logger', async () => {
+    tracer = new Tracer();
+    await tracer.init();
+    sinon.replace(fs, 'close', async (...args: any[]) => {
+      assert.fail('should not close log file');
+    });
+    tracer.destroy();
+    tracer = undefined;
+  });
+  it('#destory logger with logger', async () => {
+    let called = false;
+    sinon.replace(fs, 'close', async (...args: any[]) => {
+      called = true;
+      assert.equal(args[0], 1);
+    });
+    tracerWithFile.destroy();
+    assert.ok(called);
+    tracerWithFile = undefined;
+  });
+});
+
+describe('test the trace manager service', () => {
+  let id: string;
+  it('#create trace without log file', async () => {
+    const traceManager: TraceManager = await app.applicationContext.getAsync<TraceManager>('traceManager');
+    const tracer = await traceManager.create();
+    id  = tracer.id;
+  });
+
+  it('#destory trace without log file', async () => {
+    const traceManager: TraceManager = await app.applicationContext.getAsync<TraceManager>('traceManager');
+    traceManager.destroy(id);
+  });
+
+  it('#create trace with log file', async () => {
+    const traceManager: TraceManager = await app.applicationContext.getAsync<TraceManager>('traceManager');
+    const tracer = await traceManager.create(opts);
+    id = tracer.id;
+    assert.ok(await fs.pathExists(opts.stderrFile));
+    assert.ok(await fs.pathExists(opts.stdoutFile));
+  });
+
+  it('#get tracer', async () => {
+    const traceManager: TraceManager = await app.applicationContext.getAsync<TraceManager>('traceManager');
+    const trace = traceManager.get(id);
+    assert.equal(trace.id, id);
+  });
+
+  it('#destory trace without log file', async () => {
+    const traceManager: TraceManager = await app.applicationContext.getAsync<TraceManager>('traceManager');
+    traceManager.destroy(id);
+    return removeLogs();
+  });
+
+  it('#test trace', async () => {
+    const traceManager: TraceManager = await app.applicationContext.getAsync<TraceManager>('traceManager');
+    const id = (await traceManager.create(opts)).id;
+    const tracer = await traceManager.get(id);
+    assert.equal(tracer.id, id);
+    let logInfoFlag = false;
+    let logWarnFlag = false;
+    let logErrorFlag = false;
+    let eventStartFlag = false;
+    let eventEndFlag = false;
+    const mockLog = {
+      info: 'info message',
+      warn: 'warn message',
+      error: 'error message'
+    };
+    tracer.listen((data) => {
+      if (data.type === 'log') {
+        const logData = data as LogEvent;
+        if (logData.data.level === 'info') {
+          logInfoFlag = true;
+          assert.equal(logData.data.data, mockLog.info);
+        } else if (logData.data.level === 'warn') {
+          logWarnFlag = true;
+          assert.equal(logData.data.data, mockLog.warn);
+        } else if (logData.data.level === 'error') {
+          logErrorFlag = true;
+          assert.equal(logData.data.data, mockLog.error);
+        } else {
+          assert.fail(logData.data.level);
+        }
+      } else if (data.type) {
+        const eventData = data as JobStatusChangeEvent;
+        assert.equal(eventData.data.jobStatus, 1);
+        if (eventData.data.stepAction === 'start') {
+          eventStartFlag = true;
+          assert.equal(eventData.data.step, 'dataCollect');
+          assert.equal(eventData.data.queueLength, 1);
+        } if (eventData.data.stepAction === 'end') {
+          eventEndFlag = true;
+          assert.equal(eventData.data.step, 'dataAccess');
+          assert.equal(eventData.data.queueLength, undefined);
+        }
+      }
+    });
+    tracer.dispatch(new JobStatusChangeEvent(
+      1,
+      'dataCollect',
+      'start',
+      1
+    ));
+    tracer.dispatch(new JobStatusChangeEvent(
+      1,
+      'dataAccess',
+      'end'
+    ));
+    const loggers = tracer.getLogger();
+    loggers.stdout.write(`${mockLog.info}\n`);
+    loggers.stderr.write(`${mockLog.warn}\n`);
+    tracer.destroy(new TypeError(mockLog.error));
+    await tracer.wait();
+    assert.ok(logInfoFlag && logWarnFlag && logErrorFlag && eventStartFlag && eventEndFlag);
+    assert.ok(await fs.pathExists(opts.stderrFile) && await fs.pathExists(opts.stdoutFile));
+    const stdoutContent = await fs.readFile(opts.stdoutFile, 'utf8');
+    const stderrContent = await fs.readFile(opts.stderrFile, 'utf8');
+    assert.ok(stdoutContent.indexOf(mockLog.info) === 0);
+    assert.ok(stderrContent.indexOf(mockLog.warn) === 0);
+    return removeLogs();
+  });
+});
