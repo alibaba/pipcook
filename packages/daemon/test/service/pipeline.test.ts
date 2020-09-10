92b70be11f22b20ce99d361caca5962d7a623471
feely <feely@outlook.com>
2020-09-10 15:19:58 +0800

    fix

--- packages/daemon/test/service/pipeline.test.ts
+++ packages/daemon/test/service/pipeline.test.ts
@@ -1,4 +1,4 @@
-import { app } from 'midway-mock/bootstrap';
+import { app, assert } from 'midway-mock/bootstrap';
 import { PipelineService } from '../../src/service/pipeline';
 
 const mockPipeline = {
@@ -43,34 +43,34 @@ describe('test the pipeline service', () => {
     const jobx = await pipeline.getJobById(job.id);
     console.log(jobx);
   });
-  // it('#create pipeline and get created pipeline', async () => {
-  //   const pipeline: PipelineService = await app.applicationContext.getAsync<PipelineService>('pipelineService');
-  //   const obj = await pipeline.createPipeline(mockPipeline);
+  it('#create pipeline and get created pipeline', async () => {
+    const pipeline: PipelineService = await app.applicationContext.getAsync<PipelineService>('pipelineService');
+    const obj = await pipeline.createPipeline(mockPipeline);
 
-  //   const p1 = await pipeline.getPipeline(obj.id);
-  //   assert(p1.id === obj.id, 'found the pipeline by created id');
-  //   assert(p1.dataCollect === 'dataCollect');
-  //   await pipeline.removePipelineById(obj.id);
+    const p1 = await pipeline.getPipeline(obj.id);
+    assert.ok(p1.id === obj.id, 'found the pipeline by created id');
+    assert.ok(p1.dataCollect === 'dataCollect');
+    await pipeline.removePipelineById(obj.id);
     
-  //   console.log('removed and query');
-  //   const notExists = await pipeline.getPipeline(obj.id);
-  //   assert(notExists == null);
-  // });
+    console.log('removed and query');
+    const notExists = await pipeline.getPipeline(obj.id);
+    assert.ok(notExists == null);
+  });
 
-  // it('#update pipeline', async () => {
-  //   const pipeline: PipelineService = await app.applicationContext.getAsync<PipelineService>('pipelineService');
-  //   const obj = await pipeline.createPipeline(mockPipeline);
+  it('#update pipeline', async () => {
+    const pipeline: PipelineService = await app.applicationContext.getAsync<PipelineService>('pipelineService');
+    const obj = await pipeline.createPipeline(mockPipeline);
 
-  //   await pipeline.updatePipelineById(obj.id, {
-  //     dataCollect: 'updated',
-  //     modelTrain: 'updated'
-  //   });
+    await pipeline.updatePipelineById(obj.id, {
+      dataCollect: 'updated',
+      modelTrain: 'updated'
+    });
 
-  //   const p1 = await pipeline.getPipeline(obj.id);
-  //   assert(p1.dataCollect === 'updated');
-  //   assert(p1.modelTrain === 'updated');
+    const p1 = await pipeline.getPipeline(obj.id);
+    assert.ok(p1.dataCollect === 'updated');
+    assert.ok(p1.modelTrain === 'updated');
 
-  //   // clean
-  //   await pipeline.removePipelineById(obj.id);
-  // });
+    // clean
+    await pipeline.removePipelineById(obj.id);
+  });
 });
