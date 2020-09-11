ce7d59e34efbdc0eae0ef72076650ab571bab72d
gindis <flyxiang228@gmail.com>
2020-09-11 17:39:35 +0800

    refactor: optimize daemon startup prompt

--- packages/cli/src/bin/pipcook-daemon.ts
+++ packages/cli/src/bin/pipcook-daemon.ts
@@ -36,7 +36,7 @@ async function start(): Promise<void> {
     if (message.event === 'ready') {
       daemon.disconnect();
       daemon.unref();
-      logger.success('Pipcook service started, please visit https://pipboard.vercel.app');
+      logger.success(`Pipcook service started on ${message.data.listen}. To open pipboard, please visit https://pipboard.vercel.app`);
     }
   });
   daemon.on('exit', async (code: number) => {
