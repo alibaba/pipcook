1edf04efc8111d8d420bef69ecc9a2379583f8c9
WenheLI <wl1508@nyu.edu>
2020-10-09 21:25:00 +0800

    add

--- packages/daemon/src/service/pipeline.ts
+++ packages/daemon/src/service/pipeline.ts
@@ -21,8 +21,6 @@ import { Tracer, JobStatusChangeEvent } from './trace-manager';
 import { pluginQueue } from '../utils';
 import { UpdateParameter } from '../interface/pipeline';
 
-const boa = require('@pipcook/boa');
-
 interface SelectJobsFilter {
   pipelineId?: string;
 }
@@ -319,65 +317,65 @@ export class PipelineService {
     return path.join(CoreConstants.PIPCOOK_RUN, id, 'output.tar.gz');
   }
 
-  private _generateWASMOutput(dist: string, opts: GenerateOptions, fileQueue: Array<Promise<void | string>>): void {
-    const relay = boa.import('tvm.relay');
-    const emcc = boa.import('tvm.contrib.emcc');
-    const keras = boa.import('tensorflow.keras');
-    const {dict, open} = boa.builtins();
+  // private _generateWASMOutput(dist: string, opts: GenerateOptions, fileQueue: Array<Promise<void | string>>): void {
+  //   const relay = boa.import('tvm.relay');
+  //   const emcc = boa.import('tvm.contrib.emcc');
+  //   const keras = boa.import('tensorflow.keras');
+  //   const {dict, open} = boa.builtins();
 
-    // download tvm runtime from oss
-    const tvmjsPromise = execAsync(`wget http://ai-sample.oss-cn-hangzhou.aliyuncs.com/tvmjs/dist/tvmjs.bundle.js`, {cwd: dist});
-    fileQueue.push(tvmjsPromise);
+  //   // download tvm runtime from oss
+  //   const tvmjsPromise = execAsync(`wget http://ai-sample.oss-cn-hangzhou.aliyuncs.com/tvmjs/dist/tvmjs.bundle.js`, {cwd: dist});
+  //   fileQueue.push(tvmjsPromise);
 
-    const model = keras.models.load_model(path.join(opts.modelPath, 'model.h5'));
+  //   const model = keras.models.load_model(path.join(opts.modelPath, 'model.h5'));
 
-    const inputName = 'input_1';
-    const inputShape = model.layers[0].input_shape[0];
-    const shape = [1];
-    shape.push(inputShape[3]);
-    shape.push(inputShape[1]);
-    shape.push(inputShape[2]);
+  //   const inputName = 'input_1';
+  //   const inputShape = model.layers[0].input_shape[0];
+  //   const shape = [1];
+  //   shape.push(inputShape[3]);
+  //   shape.push(inputShape[1]);
+  //   shape.push(inputShape[2]);
 
-    const [ mod, params ] = relay.frontend.from_keras(model, dict(boa.kwargs({[inputName]: shape})));
-    const [ graph, lib, param ] = relay.build(mod, boa.kwargs({
-      params,
-      target: 'llvm -mtriple=wasm32--unknown-emcc -system-lib'
-    }));
+  //   const [ mod, params ] = relay.frontend.from_keras(model, dict(boa.kwargs({[inputName]: shape})));
+  //   const [ graph, lib, param ] = relay.build(mod, boa.kwargs({
+  //     params,
+  //     target: 'llvm -mtriple=wasm32--unknown-emcc -system-lib'
+  //   }));
 
-    lib.save(path.join(dist, 'model.bc'));
+  //   lib.save(path.join(dist, 'model.bc'));
 
-    const jsonWriter = open(path.join(dist, 'modelDesc.json'), 'w');
-    jsonWriter.write(graph);
-    const paramWriter = open(path.join(dist, 'modelParams.parmas'), 'wb');
-    paramWriter.write(relay.save_param_dict(param));
-    emcc.create_tvmjs_wasm(path.join(dist, 'model.wasi.js'), path.join(dist, 'model.bc'), boa.kwargs({
-      options: ['-O3', '-std=c++14', '-Wno-ignored-attributes', '-s', 'ALLOW_MEMORY_GROWTH=1', '-s', 'STANDALONE_WASM=1', '-s', 'ERROR_ON_UNDEFINED_SYMBOLS=0', '-s', 'ASSERTIONS=1', '--no-entry', '--pre-js', './packages/daemon/binary/preload.js']
-    }));
+  //   const jsonWriter = open(path.join(dist, 'modelDesc.json'), 'w');
+  //   jsonWriter.write(graph);
+  //   const paramWriter = open(path.join(dist, 'modelParams.parmas'), 'wb');
+  //   paramWriter.write(relay.save_param_dict(param));
+  //   emcc.create_tvmjs_wasm(path.join(dist, 'model.wasi.js'), path.join(dist, 'model.bc'), boa.kwargs({
+  //     options: ['-O3', '-std=c++14', '-Wno-ignored-attributes', '-s', 'ALLOW_MEMORY_GROWTH=1', '-s', 'STANDALONE_WASM=1', '-s', 'ERROR_ON_UNDEFINED_SYMBOLS=0', '-s', 'ASSERTIONS=1', '--no-entry', '--pre-js', './packages/daemon/binary/preload.js']
+  //   }));
 
-    const templateHead = `function EmccWASI() {`;
-    const templateTail = `
-      this.Module = Module;
-      this.start = Module.wasmLibraryProvider.start;
-      this.imports = Module.wasmLibraryProvider.imports;
-      this.wasiImport = this.imports['wasi_snapshot_preview1'];
-    }
+  //   const templateHead = `function EmccWASI() {`;
+  //   const templateTail = `
+  //     this.Module = Module;
+  //     this.start = Module.wasmLibraryProvider.start;
+  //     this.imports = Module.wasmLibraryProvider.imports;
+  //     this.wasiImport = this.imports['wasi_snapshot_preview1'];
+  //   }
 
-    if (typeof module !== 'undefined' && module.exports) {
-      module.exports = EmccWASI;
-    }
-    `;
+  //   if (typeof module !== 'undefined' && module.exports) {
+  //     module.exports = EmccWASI;
+  //   }
+  //   `;
 
-    const result = templateHead + open(path.join(dist, 'model.wasi.js')).read() + templateTail;
-    const resultWriter = open(path.join(dist, 'model.wasi.js'), 'w');
-    resultWriter.write(result);
+  //   const result = templateHead + open(path.join(dist, 'model.wasi.js')).read() + templateTail;
+  //   const resultWriter = open(path.join(dist, 'model.wasi.js'), 'w');
+  //   resultWriter.write(result);
 
-    const jsonPromise = fs.writeJSON(path.join(dist, 'modelSpec.json'), {
-      shape,
-      inputName
-    });
+  //   const jsonPromise = fs.writeJSON(path.join(dist, 'modelSpec.json'), {
+  //     shape,
+  //     inputName
+  //   });
 
-    fileQueue.push(jsonPromise);
-  }
+  //   fileQueue.push(jsonPromise);
+  // }
 
   /**
    * Generate the output package for a given job.
@@ -393,9 +391,9 @@ export class PipelineService {
     const fileQueue: Array<Promise<void | string>> = new Array();
 
     // Only support tensorflow at this moment.
-    if (opts.template === 'wasm' && opts.modelPlugin.name.includes('tensorflow')) {
-      this._generateWASMOutput(dist, opts, fileQueue);
-    }
+    // if (opts.template === 'wasm' && opts.modelPlugin.name.includes('tensorflow')) {
+    //   this._generateWASMOutput(dist, opts, fileQueue);
+    // }
 
     await execAsync('npm init -y', { cwd: dist });
 
