d182541ffcb1894692b8744289288a4073feaf01
queyue.crk <queyue.crk@alibaba-inc.com>
2020-10-09 10:42:14 +0800

    database migration

--- packages/daemon/src/migrations/20201009119293-addMd5.ts
+++ packages/daemon/src/migrations/20201009119293-addMd5.ts
@@ -7,22 +7,10 @@ export default {
       const tbNames = await queryInterface.showAllTables();
       if (tbNames.indexOf('plugins') >= 0) {
         const columns = await queryInterface.describeTable('plugins');
-        return Promise.all([
-          !columns['md5'] && queryInterface.addColumn('plugins', 'md5', {
-            type: DataTypes.STRING,
-            allowNull: true
-          }, { transaction: t })
-        ]);
-      }
-    });
-  },
-
-  down: (queryInterface: QueryInterface) => {
-    return queryInterface.sequelize.transaction(async t => {
-      const tbNames = await queryInterface.showAllTables();
-      if (tbNames.indexOf('plugins') >= 0) {
-        const columns = await queryInterface.describeTable('plugins');
-        !columns['status'] && await queryInterface.removeColumn('plugins', 'md5', { transaction: t });
+        return !columns['md5'] && queryInterface.addColumn('plugins', 'md5', {
+          type: DataTypes.STRING,
+          allowNull: true
+        }, { transaction: t });
       }
     });
   }
