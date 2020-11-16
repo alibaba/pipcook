4dc5cf4a3d31a74f8d293ab630e27769867ebb2f
feely <feely@outlook.com>
2020-11-16 21:45:07 +0800

    fixup

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-rm -rf .nyc_output coverage && npx lerna run cov && find packages -name .nyc_output | xargs -I {} cp -r {}/ ./.nyc_output
+rm -rf .nyc_output coverage
+find packages -name .nyc_output
+npx lerna run cov && find packages -name .nyc_output | xargs -I {} cp -r {}/ ./.nyc_output
+ls ./.nyc_output/
