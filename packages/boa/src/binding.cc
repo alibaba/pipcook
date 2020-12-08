9e343dedc39860018ee943346f7ca7b900ecd331
yorkie <yorkiefixer@gmail.com>
2020-12-08 15:49:11 +0800

    boa: tweak the lines via clang-format

--- packages/boa/src/binding.cc
+++ packages/boa/src/binding.cc
@@ -4,7 +4,7 @@
 #include <dlfcn.h>
 #include <napi.h>
 
-#define _CONCAT_NX(NAME, POSTFIX) lib ## NAME ## POSTFIX
+#define _CONCAT_NX(NAME, POSTFIX) lib##NAME##POSTFIX
 #define _CONCAT(NAME, POSTFIX) _CONCAT_NX(NAME, POSTFIX)
 #define _STRINGIZE_NX(S) #S
 #define _STRINGIZE(S) _STRINGIZE_NX(S)
@@ -12,11 +12,11 @@
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   // Preload the Python library for resolving numpy tracky issue.
 #if defined(__APPLE__) || defined(__MACH__)
-  char const* libpython = _STRINGIZE(_CONCAT(BOA_LIBPYTHON_NAME, .dylib));
+  char const *libpython = _STRINGIZE(_CONCAT(BOA_LIBPYTHON_NAME, .dylib));
 #elif defined(__linux__) || defined(__unix__)
-  char const* libpython = _STRINGIZE(_CONCAT(BOA_LIBPYTHON_NAME, .so));
+  char const *libpython = _STRINGIZE(_CONCAT(BOA_LIBPYTHON_NAME, .so));
 #endif
-  void* r = dlopen(libpython, RTLD_LAZY | RTLD_GLOBAL);
+  void *r = dlopen(libpython, RTLD_LAZY | RTLD_GLOBAL);
   if (r == NULL) {
     Error::New(env, "dlopen libpython failed.").ThrowAsJavaScriptException();
     return exports;
