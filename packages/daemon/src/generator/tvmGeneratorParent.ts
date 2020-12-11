6be6e3735b05de91baacbfe0d99e8f51242241b7
WenheLI <wl1508@nyu.edu>
2020-12-11 11:01:17 +0800

    add

--- packages/daemon/src/generator/tvmGeneratorParent.ts
+++ packages/daemon/src/generator/tvmGeneratorParent.ts
@@ -1,8 +1,11 @@
 import { fork } from 'child_process';
 import { GenerateOptions } from '../service/pipeline';
 import * as path from 'path';
+import { existsSync } from 'fs-extra';
 
 export function tvmGenerator(dist: string, projPackage: any, opts: GenerateOptions) {
+  // only support keras for now; Other formats will be supported in the coming commits
+  if (! existsSync(path.join(opts.modelPath, 'model.h5'))) return;
   return new Promise((resolve, reject) => {
     const client = fork(`${path.resolve(__dirname, 'tvmGenerator')}`, [JSON.stringify({
       dist,
