1cebad5bf58e25076049d485ee8d5169fe657b9f
WenheLI <wl1508@nyu.edu>
2020-12-11 11:13:28 +0800

    fix lint

--- packages/daemon/src/generator/tvmGenerator.ts
+++ packages/daemon/src/generator/tvmGenerator.ts
@@ -4,9 +4,9 @@ const validModel = ['keras'];
 
 const inputModel = process.argv[process.argv.length - 1];
 if (validModel.includes(inputModel)) {
-  const {dist, projPackage, opts, inputLayers=''} = JSON.parse(process.argv[process.argv.length - 2]);
+  const {dist, projPackage, opts, inputLayers = ''} = JSON.parse(process.argv[process.argv.length - 2]);
   if (inputModel === 'keras') {
-   keras2Wasm(dist, projPackage, opts, inputLayers); 
+   keras2Wasm(dist, projPackage, opts, inputLayers);
   }
 } else {
   console.error(`The convert type is ${inputModel} but only ${validModel.join(', ')} are available`);
--- packages/daemon/src/generator/tvmGeneratorParent.ts
+++ packages/daemon/src/generator/tvmGeneratorParent.ts
@@ -5,7 +5,7 @@ import { existsSync } from 'fs-extra';
 
 export function tvmGenerator(dist: string, projPackage: any, opts: GenerateOptions) {
   // only support keras for now; Other formats will be supported in the coming commits
-  if (! existsSync(path.join(opts.modelPath, 'model.h5'))) return;
+  if (! existsSync(path.join(opts.modelPath, 'model.h5'))) { return ; }
   return new Promise((resolve, reject) => {
     const client = fork(`${path.resolve(__dirname, 'tvmGenerator')}`, [JSON.stringify({
       dist,
