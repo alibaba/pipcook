84d777643d9798021c353c82210cfbfba97f4422
eric.lwh <eric.lwh@alibaba-inc.com>
2021-01-25 21:58:09 +0800

    add model check

--- packages/daemon/src/generator/tvm.ts
+++ packages/daemon/src/generator/tvm.ts
@@ -1,8 +1,12 @@
 import { fork } from 'child_process';
 import { GenerateOptions } from '../services/interface';
 import * as path from 'path';
+import * as fs from 'fs-extra';
 
-export function generateTVM(dist: string, projPackage: any, opts: GenerateOptions): Promise<void> {
+export async function generateTVM(dist: string, projPackage: any, opts: GenerateOptions): Promise<void> {
+  const files  = await fs.readdir(opts.modelPath);
+  // exit if no h5 file in the folder
+  if (files.filter((it) => it.endsWith('h5')).length) return;
   return new Promise((resolve, reject) => {
     const client = fork(`${path.resolve(__dirname, 'tvm.cli')}`, [ JSON.stringify({
       dist,
