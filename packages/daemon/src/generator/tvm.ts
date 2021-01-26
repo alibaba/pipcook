eee7c8ed17e9ff1b7a4c7029937a8f6c7a94e440
eric.lwh <eric.lwh@alibaba-inc.com>
2021-01-26 14:34:21 +0800

    change logic

--- packages/daemon/src/generator/tvm.ts
+++ packages/daemon/src/generator/tvm.ts
@@ -6,7 +6,8 @@ import * as fs from 'fs-extra';
 export async function generateTVM(dist: string, projPackage: any, opts: GenerateOptions): Promise<void> {
   const files = await fs.readdir(opts.modelPath);
   // exit if no h5 file in the folder
-  if (files.filter((it) => it.endsWith('h5')).length) return;
+  console.log(files.filter((it) => it.endsWith('h5')).length);
+  if (!files.filter((it) => it.endsWith('h5')).length) return;
   return new Promise((resolve, reject) => {
     const client = fork(`${path.resolve(__dirname, 'tvm.cli')}`, [ JSON.stringify({
       dist,
