027470afbd4be101e6f7fad0c75c13feb262cdef
eric.lwh <eric.lwh@alibaba-inc.com>
2021-03-18 11:28:58 +0800

    fix lint

--- packages/cli/src/utils/plugin.test.ts
+++ packages/cli/src/utils/plugin.test.ts
@@ -1,25 +1,24 @@
-import { Artifact, PipelineMeta } from '@pipcook/pipcook-core';
+import { PipelineMeta } from '@pipcook/pipcook-core';
 import test from 'ava';
 import sinon = require('sinon');
 import { extractVersion, prepareArtifactPlugin, install } from './plugin';
-import * as plugin from './plugin';
 import * as path from 'path';
 import * as fs from 'fs-extra';
 import * as utils from './index';
 
-// test('extract version', (t) => {
-//   const name = 'plugin-name';
-//   const nameScope = '@scope/plugin-name';
-//   t.deepEqual(extractVersion(name), { name, version: 'latest' });
-//   t.deepEqual(extractVersion(nameScope), { name: nameScope, version: 'latest' });
-//   t.deepEqual(extractVersion(`${name}@1.1.0`), { name, version: '1.1.0' });
-//   t.deepEqual(extractVersion(`${name}@beta`), { name, version: 'beta' });
-//   t.deepEqual(extractVersion(`${name}@alpha`), { name, version: 'alpha' });
-//   t.deepEqual(extractVersion(`${nameScope}@latest`), { name: nameScope, version: 'latest' });
-//   t.deepEqual(extractVersion(`${nameScope}@beta`), { name: nameScope, version: 'beta' });
-//   t.deepEqual(extractVersion(`${nameScope}@alpha`), { name: nameScope, version: 'alpha' });
-//   t.deepEqual(extractVersion(`${nameScope}@latest`), { name: nameScope, version: 'latest' });
-// });
+test('extract version', (t) => {
+  const name = 'plugin-name';
+  const nameScope = '@scope/plugin-name';
+  t.deepEqual(extractVersion(name), { name, version: 'latest' });
+  t.deepEqual(extractVersion(nameScope), { name: nameScope, version: 'latest' });
+  t.deepEqual(extractVersion(`${name}@1.1.0`), { name, version: '1.1.0' });
+  t.deepEqual(extractVersion(`${name}@beta`), { name, version: 'beta' });
+  t.deepEqual(extractVersion(`${name}@alpha`), { name, version: 'alpha' });
+  t.deepEqual(extractVersion(`${nameScope}@latest`), { name: nameScope, version: 'latest' });
+  t.deepEqual(extractVersion(`${nameScope}@beta`), { name: nameScope, version: 'beta' });
+  t.deepEqual(extractVersion(`${nameScope}@alpha`), { name: nameScope, version: 'alpha' });
+  t.deepEqual(extractVersion(`${nameScope}@latest`), { name: nameScope, version: 'latest' });
+});
 
 test.serial.afterEach(() => sinon.restore());
 
@@ -30,12 +29,12 @@ test.serial('should install', async (t) => {
     name: 'test',
     version: 'latest'
   };
-  const expected =  path.join(homeDir, 'node_modules', `${extractRes.name}-${extractRes.version}`);
+  const expected = path.join(homeDir, 'node_modules', `${extractRes.name}-${extractRes.version}`);
 
   const stubPathExists = sinon.stub(fs, 'pathExists').resolves(true);
   const stubMkdirp = sinon.stub(fs, 'mkdirp').resolves();
   const stubExecAsync = sinon.stub(utils, 'execAsync').resolves();
-  
+
   const requirePath = await install(name, homeDir);
 
   t.true(stubPathExists.calledOnce);
@@ -51,7 +50,7 @@ test.serial('should install and create folder', async (t) => {
     name: 'test',
     version: 'latest'
   };
-  const expected =  path.join(homeDir, 'node_modules', `${extractRes.name}-${extractRes.version}`);
+  const expected = path.join(homeDir, 'node_modules', `${extractRes.name}-${extractRes.version}`);
 
   const stubPathExists = sinon.stub(fs, 'pathExists').resolves(false);
   const stubMkdirp = sinon.stub(fs, 'mkdirp').resolves();
@@ -69,7 +68,7 @@ test.serial('should not prepare artifact plugin', async (t) => {
   const pipelineMeta: PipelineMeta = {
     specVersion: 'test',
     dataSource: 'test',
-    dataflow: ['test'],
+    dataflow: [ 'test' ],
     model: 'test',
     artifacts: undefined,
     options: {}
@@ -84,7 +83,7 @@ test.serial('should prepare artifact plugin', async (t) => {
   const pipelineMeta: PipelineMeta = {
     specVersion: 'test',
     dataSource: 'test',
-    dataflow: ['test'],
+    dataflow: [ 'test' ],
     model: 'test',
     artifacts: [],
     options: {}
