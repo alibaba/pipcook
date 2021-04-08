3b11d2426bf701d10d1247f31fcb66496366e81f
feely <feely@outlook.com>
2021-04-08 16:58:40 +0800

    boa: move boa to imgcook/boa

--- packages/boa/tests/es-module-loaders/js/test-esm-loader-math.mjs
+++
@@ -1,8 +0,0 @@
-const math = {
-  add: (a, b) => a + b,
-  subtract: (a, b) => a - b
-};
-
-export const add = math.add;
-export const subtract = math.subtract;
-export default math;
--- packages/boa/tests/numpy/ufunc.js
+++
@@ -1,9 +0,0 @@
-const test = require('ava');
-const boa = require('../../');
-const np = boa.import('numpy');
-
-test('the ndarry constructor', t => {
-  console.log(`${np.typecodes['All']}`);
-  t.pass();
-});
-
