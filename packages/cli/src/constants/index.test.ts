a7b4702a7a424a1581ed5e7699bd0d76fbdde6b8
feely <feely@outlook.com>
2021-04-13 19:49:13 +0800

    cli: fix coverage of constants (#766)

--- packages/cli/src/constants/index.test.ts
+++ packages/cli/src/constants/index.test.ts
@@ -1,6 +1,14 @@
 import test from 'ava';
-import { PIPCOOK_FRAMEWORK_MIRROR_BASE } from './';
+import * as path from 'path';
+import { homedir } from 'os';
+import * as constants from './';
 
-test('PIPCOOK_FRAMEWORK_MIRROR_BASE', (t) => {
-  t.is(PIPCOOK_FRAMEWORK_MIRROR_BASE, 'https://pipcook-cloud.oss-cn-hangzhou.aliyuncs.com/framework/');
+test('constants of cli', (t) => {
+  t.is(constants.PIPCOOK_FRAMEWORK_MIRROR_BASE, 'https://pipcook-cloud.oss-cn-hangzhou.aliyuncs.com/framework/');
+  t.is(constants.FrameworkDescFilename, 'framework.json');
+  t.is(constants.PIPCOOK_PLUGIN_ARTIFACT_PATH, path.join(constants.PIPCOOK_HOME_PATH, 'artifact'));
+  t.is(constants.PIPCOOK_HOME_PATH, path.join(homedir(), '.pipcook'));
+  t.is(constants.PIPCOOK_TMPDIR, path.join(constants.PIPCOOK_HOME_PATH, '.tmp'));
+  t.is(constants.PIPCOOK_FRAMEWORK_PATH, path.join(constants.PIPCOOK_HOME_PATH, 'framework'));
+  t.is(constants.PIPCOOK_SCRIPT_PATH, path.join(constants.PIPCOOK_HOME_PATH, 'script'));
 });
