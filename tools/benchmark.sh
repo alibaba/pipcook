2eac61b483424e176b96772f2f530145e3f0370f
feely <feiyu.zfy@alibaba-inc.com>
2021-09-14 11:53:04 +0800

    meta: fix benchmark tool (#863)

--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -22,41 +22,31 @@ t2=$(date +%s)
 build_time=$((t2-t1))
 
 t1=$(date +%s)
-./packages/cli/dist/bin/pipcook init
-t2=$(date +%s)
-init_time=$((t2-t1))
-
-./packages/cli/dist/bin/pipcook daemon start
-t1=$(date +%s)
 npm run test
 t2=$(date +%s)
 test_time=$((t2-t1))
 
 t1=$(date +%s)
-./packages/cli/dist/bin/pipcook run ./example/pipelines/mnist-image-classification.json
+./packages/cli/dist/bin/pipcook train ./example/pipelines/image-classification-mobilenet.json
 t2=$(date +%s)
-mnist_time=$((t2-t1))
+mobilenet_time=$((t2-t1))
 
 if [ -z ${UPLOAD} ]
 then
-  echo "{\"install_time\":${install_time}, \"init_time\":$init_time, \
-  \"esbuild_time\":$esbuild_time, \"build_time\":$build_time, \"test_time\":$test_time, \
-  \"mnist_time\":$mnist_time, \"timestamp\": $time_stamp }" | jq
+  echo "{\"install_time\":${install_time}, \"build_time\":$build_time, \"test_time\":$test_time, \
+  \"mobilenet_time\":$mobilenet_time, \"timestamp\": $time_stamp }" | jq
 else 
   git clone https://github.com/imgcook/pipcook-benchmark.git
   cd pipcook-benchmark
 
   echo $(cat data.json | 
           jq --arg install_time $install_time \
-            --arg esbuild_time $esbuild_time \
             --arg build_time $build_time \
-            --arg init_time $init_time \
             --arg test_time $test_time \
             --arg time_stamp $(date +%s) \
-            --arg mnist_time $mnist_time \
-            ". + [{commitId: $CIRCLE_SHA1, install_time:$install_time, init_time:$init_time, \
-                  esbuild_time:$esbuild_time, build_time:$build_time, test_time:$test_time, \
-                  mnist_time:$mnist_time, timestamp: $time_stamp }]")  > data.json
+            --arg mobilenet_time $mobilenet_time \
+            ". + [{commitId: $CIRCLE_SHA1, install_time:$install_time, build_time:$build_time, test_time:$test_time, \
+                  mobilenet_time:$mobilenet_time, timestamp: $time_stamp }]")  > data.json
 
   git config user.email ${EMAIL}
   git config user.name ${USERNAME}
