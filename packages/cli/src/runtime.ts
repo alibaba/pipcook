bf4cdf3d2d361366dd9b5d8ff7653fddb3d97519
feely <feiyu.zfy@alibaba-inc.com>
2021-09-18 16:25:16 +0800

    change workspace

--- packages/cli/src/runtime.ts
+++ packages/cli/src/runtime.ts
@@ -111,10 +111,8 @@ export class StandaloneRuntime {
     const modulePath = path.join(this.workspace.frameworkDir, constants.JSModuleDirName);
     const scriptModulePath = path.join(this.scriptDir, constants.JSModuleDirName);
     if (await fs.pathExists(modulePath) && !await fs.pathExists(scriptModulePath)) {
-      // remove if it exists
-      await fs.remove(scriptModulePath);
       // link node_module in framework to script directory
-      await fs.symlink(modulePath, scriptModulePath);
+      await fs.ensureSymlink(modulePath, scriptModulePath);
     }
     // link @pipcook/core to node_module
     await Script.linkCoreToScript(scriptModulePath);
