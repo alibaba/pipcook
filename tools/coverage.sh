1ceaa2c4a26029bf998afb2ea585d43742a06f7d
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-19 10:37:54 +0800

    Delete tools directory

--- tools/coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# clear old data
-rm -rf .nyc_output coverage && \
-# generate coverage file
-npx lerna run cov && \
-# merge coverage directory into package root
-find packages -type d -name .nyc_output -exec cp -r {} ./ \;
